e37559afc46766dd6194293ed7f8b2942c10f14c
ATuan <4A8K0069@stust.edu.tw>
2022-04-08 18:46:08 +0800

    added eventbus to connect Poster

--- Assets/HelloPico2/Hand/Scripts/Event/HandSelected.cs
+++ Assets/HelloPico2/Hand/Scripts/Event/HandSelected.cs
@@ -1,5 +1,15 @@
-﻿namespace HelloPico2.Hand.Scripts.Event{
+﻿using UnityEngine;
+
+namespace HelloPico2.Hand.Scripts.Event{
 	public class HandSelected{
-		
+		public GameObject SelectedObject{ get; }
+		public bool IsEnter{ get; }
+		public int SelectedInstanceID{ get; }
+
+		public HandSelected(GameObject selectedObject, bool isEnter){
+			SelectedObject = selectedObject;
+			IsEnter = isEnter;
+			SelectedInstanceID = selectedObject.GetInstanceID();
+		}
 	}
 }
