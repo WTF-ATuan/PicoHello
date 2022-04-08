f08fa67ea504328f07298559db8c41a3f3f28aa3
Atuan <jain60501@gmail.com.tw>
2022-04-08 15:51:28 +0800

    added MenuPresenter.cs to enter game Level

---
+++ Assets/HelloPico2/Menu/Interactable/Scripts/MenuPresenter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace HelloPico2.Menu.Interactable{
+	public class MenuPresenter : MonoBehaviour{
+		[SerializeField] private GameObject enterLevelTarget;
+
+		[SerializeField] private UnityEvent enterLevelViewEvent;
+
+
+		private void OnHandSelected(GameObject selectObject){
+			var instanceID = selectObject.gameObject.GetInstanceID();
+			var isEquals = enterLevelTarget.GetInstanceID().Equals(instanceID);
+			if(isEquals){
+				EnterLevel();
+			}
+		}
+
+		private void EnterLevel(){
+			enterLevelViewEvent?.Invoke();
+		}
+	}
+}
