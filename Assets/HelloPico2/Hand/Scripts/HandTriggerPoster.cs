e5f536d7c02ebcfe224ac0dd301f80833c931441
Atuan <jain60501@gmail.com.tw>
2022-04-09 15:31:45 +0800

    Hand Trigger Poster creating spheres

--- Assets/HelloPico2/Hand/Scripts/HandTriggerPoster.cs
+++ Assets/HelloPico2/Hand/Scripts/HandTriggerPoster.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace HelloPico2.Hand.Scripts{
@@ -6,6 +7,9 @@ namespace HelloPico2.Hand.Scripts{
 	//TODO need refactor event poster;
 	public class HandTriggerPoster : MonoBehaviour{
 		private Hand _hand;
+
+		//Todo roughTest
+		private Coroutine _createCoroutine;
 
 		private void Start(){
 			_hand = GetComponent<Hand>();
@@ -16,6 +20,7 @@ namespace HelloPico2.Hand.Scripts{
 			if(other.tag.Equals("TriggerPoint")){
 				var particle = _hand.palmTransform.GetComponent<ParticleSystem>();
 				particle.Play();
+				_createCoroutine = StartCoroutine(DelayCreating(2f));
 			}
 		}
 
@@ -23,7 +28,18 @@ namespace HelloPico2.Hand.Scripts{
 			if(other.tag.Equals("TriggerPoint")){
 				var particle = _hand.palmTransform.GetComponent<ParticleSystem>();
 				particle.Stop();
+				if(_createCoroutine != null){
+					StopCoroutine(_createCoroutine);
+				}
 			}
+		}
+
+		private IEnumerator DelayCreating(float delayTime){
+			yield return new WaitForSeconds(delayTime);
+			var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			sphere.transform.position = _hand.palmPoint;
+			sphere.transform.localScale = Vector3.one * 0.2f;
+			sphere.GetComponent<Collider>().enabled = false;
 		}
 	}
 }
