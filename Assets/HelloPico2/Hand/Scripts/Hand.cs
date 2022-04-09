81b6264318f5d6de16967fbd861f6ad1678d39f3
Atuan <jain60501@gmail.com.tw>
2022-04-09 14:52:21 +0800

    added particle to hand

---
+++ Assets/HelloPico2/Hand/Scripts/Hand.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace HelloPico2.Hand.Scripts{
+	public class Hand : MonoBehaviour{
+		[SerializeField] private Transform palmTransform;
+		
+		public Vector3 palmPoint => palmTransform.position;
+	}
+}
