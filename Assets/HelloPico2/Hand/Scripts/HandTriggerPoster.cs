fb4d8a8ae8f21fb46ccb311bd33939e1f4427e31
ATuan <4A8K0069@stust.edu.tw>
2022-04-11 14:57:22 +0800

    meta file save

--- Assets/HelloPico2/Hand/Scripts/HandTriggerPoster.cs
+++ Assets/HelloPico2/Hand/Scripts/HandTriggerPoster.cs
@@ -6,30 +6,30 @@ namespace HelloPico2.Hand.Scripts{
 	[RequireComponent(typeof(Hand))]
 	//TODO need refactor event poster;
 	public class HandTriggerPoster : MonoBehaviour{
-		private Hand _hand;
+		private Hand hand;
 
 		//Todo roughTest
-		private Coroutine _createCoroutine;
+		private Coroutine createCoroutine;
 
 		private void Start(){
-			_hand = GetComponent<Hand>();
+			hand = GetComponent<Hand>();
 		}
 
 		//TODO need refactor 
 		private void OnTriggerEnter(Collider other){
 			if(other.tag.Equals("TriggerPoint")){
-				var particle = _hand.palmTransform.GetComponent<ParticleSystem>();
+				var particle = hand.palmTransform.GetComponent<ParticleSystem>();
 				particle.Play();
-				_createCoroutine = StartCoroutine(DelayCreating(2f));
+				createCoroutine = StartCoroutine(DelayCreating(2f));
 			}
 		}
 
 		private void OnTriggerExit(Collider other){
 			if(other.tag.Equals("TriggerPoint")){
-				var particle = _hand.palmTransform.GetComponent<ParticleSystem>();
+				var particle = hand.palmTransform.GetComponent<ParticleSystem>();
 				particle.Stop();
-				if(_createCoroutine != null){
-					StopCoroutine(_createCoroutine);
+				if(createCoroutine != null){
+					StopCoroutine(createCoroutine);
 				}
 			}
 		}
@@ -37,7 +37,7 @@ namespace HelloPico2.Hand.Scripts{
 		private IEnumerator DelayCreating(float delayTime){
 			yield return new WaitForSeconds(delayTime);
 			var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			sphere.transform.position = _hand.palmPoint;
+			sphere.transform.position = hand.palmPoint;
 			sphere.transform.localScale = Vector3.one * 0.2f;
 			sphere.GetComponent<Collider>().enabled = false;
 		}
