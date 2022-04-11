ce878a94e1529ef1b91204f4aa19853b0f282b22
jeremyhuang <jeremyimage@gmail.com>
2022-04-11 14:08:21 +0800

    Revert "Revert "Revert "Merge remote-tracking branch 'origin/Develop' into Develop"""
    This reverts commit d518ce6f440fc567eafc964b73f382e63d588fef.

--- Assets/HelloPico2/Hand/Scripts/Event/HandSelected.cs
+++ Assets/HelloPico2/Hand/Scripts/Event/HandSelected.cs
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
 ﻿namespace HelloPico2.Hand.Scripts.Event{
 	public class HandSelected{
 		
-=======
-﻿using UnityEngine;
-
-namespace HelloPico2.Hand.Scripts.Event{
-	public class HandSelected{
-		public GameObject SelectedObject{ get; }
-		public bool IsEnter{ get; }
-		public int SelectedInstanceID{ get; }
-
-		public HandSelected(GameObject selectedObject, bool isEnter){
-			SelectedObject = selectedObject;
-			IsEnter = isEnter;
-			SelectedInstanceID = selectedObject.GetInstanceID();
-		}
->>>>>>> Develop
 	}
 }
