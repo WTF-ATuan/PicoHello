a7d34f61ee9a7a064f9185380f57b0786674fa97
ATuan <4A8K0069@stust.edu.tw>
2022-04-11 17:31:46 +0800

    added InteractorController.cs to control XR InteractionManager

---
+++ Assets/HelloPico2/InputDevice/Scripts/InteractorController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.XR.Interaction.Toolkit;
+
+namespace HelloPico2.InputDevice.Scripts{
+	public class InteractorController{
+		private XRBaseInteractor Interactor{ get; }
+		private XRInteractionManager Manager{ get; }
+
+		public InteractorController(XRBaseInteractor interactor, XRInteractionManager manager){
+			Interactor = interactor;
+			Manager = manager;
+		}
+
+		public void Select(IXRSelectInteractable interactable){
+			Manager.SelectEnter(Interactor, interactable);
+		}
+	}
+}
