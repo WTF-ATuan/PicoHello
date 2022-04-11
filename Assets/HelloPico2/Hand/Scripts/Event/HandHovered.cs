d518ce6f440fc567eafc964b73f382e63d588fef
jeremyhuang <jeremyimage@gmail.com>
2022-04-11 14:08:21 +0800

    Revert "Revert "Merge remote-tracking branch 'origin/Develop' into Develop""
    This reverts commit 283f4229362d686f2592b5c008bf92e138327601.

--- Assets/HelloPico2/Hand/Scripts/Event/HandHovered.cs
+++ Assets/HelloPico2/Hand/Scripts/Event/HandHovered.cs
@@ -1,5 +1,20 @@
 ﻿using UnityEngine;
 
 namespace HelloPico2.Hand.Scripts.Event{
+<<<<<<< HEAD
 	public class HandHovered{ }
+=======
+	public class HandHovered{
+		public GameObject HoveredObject{ get; }
+		public bool IsEnter{ get; }
+
+		public int HoveredInstanceID{ get; }
+
+		public HandHovered(GameObject hoveredObject, bool isEnter){
+			HoveredObject = hoveredObject;
+			IsEnter = isEnter;
+			HoveredInstanceID = hoveredObject.GetInstanceID();
+		}
+	}
+>>>>>>> Develop
 }
---
+++ Assets/HelloPico2/Menu/Interactable/Scripts/IntroPresenter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace HelloPico2.Menu.Interactable{
+	// 小精靈 聲音 ⇒ (時間點到) 觸發 手柄微幅震動  ⇒ 世界變亮  ⇒ 小精靈動畫 
+	// ⇒ (動畫時間點到) 改變玩家的手柄材質 lerp 的形式 ⇒ 小精靈動畫 
+	// ⇒ 玩家飛入門中。
+	public class IntroPresenter : MonoBehaviour{
+		
+	}
+}
