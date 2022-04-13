7beab2f2986b27e4c2e798b63cbc0b2ab254e59c
Atuan <jain60501@gmail.com.tw>
2022-04-13 17:43:51 +0800

    added IChargeable to 分離 attackable

--- Assets/HelloPico2/Interactable/Scripts/AttackableProp.cs
+++ Assets/HelloPico2/Interactable/Scripts/AttackableProp.cs
@@ -1,11 +1,35 @@
-﻿using UnityEngine;
+﻿using Game.Project;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace HelloPico2.Interactable.Scripts{
+	[RequireComponent(typeof(XRGrabInteractable))]
 	public class AttackableProp : MonoBehaviour{
+		private XRGrabInteractable _grabInteractable;
+		private ColdDownTimer _timer;
 
+		private IChargeable _chargeable;
 
-		public void Charge(){
-			
+		private void Start(){
+			_grabInteractable = GetComponent<XRGrabInteractable>();
+			_chargeable = GetComponent<IChargeable>();
+			_timer = new ColdDownTimer(_chargeable.chargeTime);
+			RegisterListener();
+		}
+
+		private void RegisterListener(){
+			_grabInteractable.selectEntered.AddListener(OnSelectEntered);
+			_grabInteractable.selectExited.AddListener(OnSelectExited);
+		}
+
+		private void OnSelectEntered(SelectEnterEventArgs arg0){
+			_timer.Reset();
+		}
+
+		private void OnSelectExited(SelectExitEventArgs arg0){
+			if(_timer.CanInvoke()){
+				_chargeable.Activate(_grabInteractable);
+			}
 		}
 	}
 }
