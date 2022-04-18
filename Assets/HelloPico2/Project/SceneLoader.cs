7cb368e8ab1822ec155456a56fd979b41449c806
Atuan <jain60501@gmail.com.tw>
2022-04-18 18:59:22 +0800

    added SceneLoader To load Scene

---
+++ Assets/HelloPico2/Project/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project{
+	public class SceneLoader : MonoBehaviour{
+		private List<AsyncOperation> _sceneToLoad;
+
+		private void Start(){
+			_sceneToLoad = new List<AsyncOperation>();
+			var currentScene = SceneManager.GetActiveScene();
+			var currentSceneName = currentScene.name;
+			var loadSceneAsync = SceneManager.LoadSceneAsync(currentSceneName);
+			_sceneToLoad.Add(loadSceneAsync);
+		}
+
+		public void AddScene(string sceneName){ }
+	}
+}
