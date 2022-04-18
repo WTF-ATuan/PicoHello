7e132fbb0bb2f61b1b20bfe2907d08dd21f5aff0
Atuan <jain60501@gmail.com.tw>
2022-04-18 19:13:06 +0800

    added two function in SceneLoader add and remove

--- Assets/HelloPico2/Project/SceneLoader.cs
+++ Assets/HelloPico2/Project/SceneLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,18 @@ namespace Project{
 	public class SceneLoader : MonoBehaviour{
 		private readonly List<AsyncOperation> _sceneToLoad = new List<AsyncOperation>();
 
-		public void AddScene(string sceneName){ }
+		public void AddScene(string sceneName){
+			var scene = SceneManager.GetSceneByName(sceneName);
+			if(scene == default) throw new Exception($"Can,t find {sceneName} check build setting");
+			var loadSceneAsync = SceneManager.LoadSceneAsync(sceneName);
+			_sceneToLoad.Add(loadSceneAsync);
+		}
+
+		public void RemoveScene(string sceneName){
+			var scene = SceneManager.GetSceneByName(sceneName);
+			if(scene == default) throw new Exception($"Can,t find {sceneName} check build setting");
+			var loadSceneAsync = SceneManager.LoadSceneAsync(sceneName);
+			_sceneToLoad.Remove(loadSceneAsync);
+		}
 	}
 }
