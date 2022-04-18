823d3be7567b1a3595107bdf9ed8b4735a660e37
Atuan <jain60501@gmail.com.tw>
2022-04-18 19:00:13 +0800

    fixed SceneLoad not include self Scene

--- Assets/HelloPico2/Project/SceneLoader.cs
+++ Assets/HelloPico2/Project/SceneLoader.cs
@@ -4,15 +4,7 @@ using UnityEngine.SceneManagement;
 
 namespace Project{
 	public class SceneLoader : MonoBehaviour{
-		private List<AsyncOperation> _sceneToLoad;
-
-		private void Start(){
-			_sceneToLoad = new List<AsyncOperation>();
-			var currentScene = SceneManager.GetActiveScene();
-			var currentSceneName = currentScene.name;
-			var loadSceneAsync = SceneManager.LoadSceneAsync(currentSceneName);
-			_sceneToLoad.Add(loadSceneAsync);
-		}
+		private readonly List<AsyncOperation> _sceneToLoad = new List<AsyncOperation>();
 
 		public void AddScene(string sceneName){ }
 	}
