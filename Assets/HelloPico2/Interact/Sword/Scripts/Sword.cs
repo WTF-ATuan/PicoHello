f800b706425720fe18983f62133723a2eb6c2ba6
Atuan <jain60501@gmail.com.tw>
2022-04-18 20:16:10 +0800

    added Sword to control blade lenght

---
+++ Assets/HelloPico2/Interact/Sword/Scripts/Sword.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
+using UnityEngine;
+
+namespace HelloPico2.Interact.Sword.Scripts{
+	public class Sword : MonoBehaviour{
+		[SerializeField] private Transform blade;
+
+		[ReadOnly] [SerializeField] private List<Transform> bladeComponentList;
+
+		private void Start(){
+			bladeComponentList = blade.GetComponentsInChildren<Transform>().ToList();
+		}
+
+		public void AddBladeComponent(){
+			//複製一份最後面的 
+			var lastComponent = bladeComponentList.Last();
+			var addedComponent = Instantiate(lastComponent, lastComponent.position, Quaternion.identity, lastComponent);
+			//加上他的Local Position
+			var lastComponentLocalPosition = lastComponent.localPosition;
+			addedComponent.localPosition = lastComponentLocalPosition;
+			bladeComponentList.Add(addedComponent);
+		}
+
+		public void RemoveBladeComponent(){
+			var lastComponent = bladeComponentList.Last();
+			bladeComponentList.Remove(lastComponent);
+			DestroyImmediate(lastComponent.gameObject);
+		}
+	}
+}
