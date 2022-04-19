c02bc8d5480b98b221f32a4401ea6ceb52eaee94
Atuan <jain60501@gmail.com.tw>
2022-04-19 12:18:40 +0800

    refactor InputDeviceTrigger.cs with Interface

---
+++ Assets/HelloPico2/InputDevice/Scripts/Event/DeviceInputEvent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace HelloPico2.Hand.Scripts.Event{
+	public interface IGrip{
+		void OnGrip(float gripValue);
+	}
+
+	public interface ITrigger{
+		void OnTrigger(float triggerValue);
+	}
+
+	public interface ITouchPad{
+		void OnTouchPad(Vector2 padAxis);
+	}
+
+	public interface IPrimaryButton{
+		void OnPrimaryButtonClick();
+	}
+
+	public interface ISecondaryButton{
+		void OnSecondaryButtonClick();
+	}
+}
--- Assets/HelloPico2/InputDevice/Scripts/InputDeviceTrigger.cs
+++ Assets/HelloPico2/InputDevice/Scripts/InputDeviceTrigger.cs
@@ -1,101 +1,57 @@
-﻿using UnityEngine;
+﻿using HelloPico2.Hand.Scripts.Event;
+using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 namespace HelloPico2.InputDevice.Scripts{
-	public class InputDeviceTrigger : MonoBehaviour
-	{
+	public class InputDeviceTrigger : MonoBehaviour{
 		private XRController _xrController;
 		private UnityEngine.XR.InputDevice _inputDevice;
-		[SerializeField] Transform[] ItemEffect;
-		private int getType=0;//null 0 ball 1,acball 2
-		private int rangeNum;
 
-		private void Start()
-		{
+		private IGrip _grip;
+		private ITrigger _trigger;
+		private ITouchPad _touchPad;
+		private IPrimaryButton _primaryButton;
+		private ISecondaryButton _secondaryButton;
+
+		private void Start(){
 			_xrController = GetComponent<XRController>();
 			_inputDevice = _xrController.inputDevice;
 
+			_grip = GetComponent<IGrip>();
+			_trigger = GetComponent<ITrigger>();
+			_touchPad = GetComponent<ITouchPad>();
+			_primaryButton = GetComponent<IPrimaryButton>();
+			_secondaryButton = GetComponent<ISecondaryButton>();
 		}
 
-		private void Update()
-		{
+		private void Update(){
 			_inputDevice.TryGetFeatureValue(CommonUsages.trigger, out var triggerValue);
 			_inputDevice.TryGetFeatureValue(CommonUsages.grip, out var gripValue);
 			_inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out var touchPadAxis);
 			_inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out var primaryButtonValue);
 			_inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out var secondaryButtonValue);
 
-			
-			if (triggerValue > 0.1f)
-			{	
-				OnTrigger(triggerValue);
+
+			if(gripValue > 0.1f){
+				_grip?.OnGrip(gripValue);
 			}
 
-			if (gripValue > 0.1f)
-			{
-				OnGrip(gripValue);
+			if(triggerValue > 0.1f){
+				_trigger?.OnTrigger(triggerValue);
 			}
 
-			if (touchPadAxis.magnitude > 0.1f)
-			{
-				OnTouchPad(touchPadAxis);
+			if(touchPadAxis.magnitude > 0.1f){
+				_touchPad?.OnTouchPad(touchPadAxis);
 			}
 
-			if (primaryButtonValue)
-			{
-				OnPrimayBtn(true);
+			if(primaryButtonValue){
+				_primaryButton?.OnPrimaryButtonClick();
 			}
 
-			if (secondaryButtonValue)
-			{
-				OnSecondBtn(true);
-			}
-			
-		}
-
-		private void OnTrigger(float triggerValue)
-		{
-			ShowEffect();
-		}
-
-		private void OnGrip(float gripVale){ }
-
-		private void OnTouchPad(Vector2 axis) { }
-
-		private void OnPrimayBtn(bool primaryButtonValue)
-		{
-			ShowEffect();
-		}
-		private void OnSecondBtn(bool secondaryButtonValue)
-		{
-			ShowEffect();
-		}
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.tag == "ball")
-            {
-				getType = 1;
-			}
-        }
-        private void OnTriggerExit(Collider other)
-        {
-			getType = 0;
-		}
-		private void  ShowEffect()
-        {
-			if (getType == 0)
-			{
-				var particle = ItemEffect[0].GetComponent<ParticleSystem>();
-				particle.Play();
-			}
-			else if (getType == 1)
-			{
-				var particle = ItemEffect[2].GetComponent<ParticleSystem>();
-				particle.Play();
+			if(secondaryButtonValue){
+				_secondaryButton?.OnSecondaryButtonClick();
 			}
 		}
-		
-		}
-		
 	}
+}
