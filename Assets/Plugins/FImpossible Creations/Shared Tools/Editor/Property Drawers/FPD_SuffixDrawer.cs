eee8a26d149936db36bed3a404b18fbbb4ccb489
Atuan <jain60501@gmail.com.tw>
2022-04-21 14:37:31 +0800

    import tail animator

--- Assets/Plugins/FImpossible Creations/Shared Tools/Editor/Property Drawers/FPD_SuffixDrawer.cs
+++ Assets/Plugins/FImpossible Creations/Shared Tools/Editor/Property Drawers/FPD_SuffixDrawer.cs
@@ -37,6 +37,10 @@ namespace FIMSpace.FEditor
             sliderVal = GUI.HorizontalSlider(position, property.floatValue, Attribute.Min, Attribute.Max);
 
             float pre, value;
+            int indent;
+
+            indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
 
             switch (Attribute.Mode)
             {
@@ -84,7 +88,7 @@ namespace FIMSpace.FEditor
                 case FPD_SuffixAttribute.SuffixMode.FromMinToMaxRounded:
 
                     pre = Mathf.Round(sliderVal);
-                    value = EditorGUI.FloatField(floatField, Mathf.Round(sliderVal) );
+                    value = EditorGUI.FloatField(floatField, Mathf.Round(sliderVal));
                     if (value != pre) sliderVal = value;
 
                     EditorGUI.LabelField(percField, Attribute.Suffix);
@@ -93,6 +97,7 @@ namespace FIMSpace.FEditor
             }
 
             property.floatValue = sliderVal;
+            EditorGUI.indentLevel = indent;
 
             EditorGUI.EndProperty();
 
