2bfa4c24e43f1ba32bff0f6680ed2bc29d1cbdc5
Atuan <jain60501@gmail.com.tw>
2022-04-26 18:17:59 +0800

    Test it,s good

---
+++ Assets/HelloPico2/Data/VFX/0.VR Project Test/Scripts/DeviceController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace HelloPico2.Data.VFX._0.VR_Project_Test.Scripts{
+	public class DeviceController : MonoBehaviour{
+		[SerializeField] private ParticleAdjuster adjuster;
+		private XRController _controller;
+		private UnityEngine.XR.InputDevice _inputDevice;
+
+		private int _rgbIndex;
+
+		private void Start(){
+			_controller = GetComponent<XRController>();
+			_inputDevice = _controller.inputDevice;
+		}
+
+		private void Update(){
+			var controllerNode = _controller.controllerNode;
+			if(controllerNode == XRNode.RightHand){
+				ChangeVelocity();
+			}
+
+			if(controllerNode == XRNode.LeftHand){
+				ChangeShaderColor();
+				ChangeRgbIndex();
+			}
+		}
+
+		private void ChangeVelocity(){
+			_inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out var touchpadAxis);
+			if(touchpadAxis.magnitude < 0.1f) return;
+			var axisX = touchpadAxis.x;
+			adjuster.ModifyVelocity(axisX);
+		}
+
+		private bool _primaryButtonDownFlag;
+		private bool _secondaryButtonDownFlag;
+
+		private void ChangeRgbIndex(){
+			_inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out var primaryButtonValue);
+			_inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out var secondaryButtonValue);
+			if(primaryButtonValue && !_primaryButtonDownFlag){
+				_rgbIndex++;
+				_primaryButtonDownFlag = true;
+			}
+			else{
+				_primaryButtonDownFlag = false;
+			}
+
+			if(secondaryButtonValue && !_secondaryButtonDownFlag){
+				_rgbIndex++;
+				_secondaryButtonDownFlag = true;
+			}
+			else{
+				_secondaryButtonDownFlag = false;
+			}
+		}
+
+		private void ChangeShaderColor(){
+			_inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out var touchpadAxis);
+			if(touchpadAxis.magnitude < 0.1f) return;
+			var axisX = touchpadAxis.x;
+			switch(_rgbIndex){
+				case 0:
+					adjuster.ModifyColor(axisX, 0, 0);
+					break;
+				case 1:
+					adjuster.ModifyColor(0, axisX, 0);
+					break;
+				case 2:
+					adjuster.ModifyColor(0, 0, axisX);
+					break;
+			}
+		}
+	}
+}
