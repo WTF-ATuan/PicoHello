76b96d63d9f9fc113461900fd22720f5dd116f25
Atuan <jain60501@gmail.com.tw>
2022-04-27 13:33:16 +0800

    Remove scripts

--- Assets/HelloPico2/Interact/Scripts/IChargeable.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine.XR.Interaction.Toolkit;
-
-namespace HelloPico2.Interactable.Scripts{
-	public interface IChargeable{
-		float chargeTime{ get; }
-		void Activate(XRGrabInteractable interactable);
-	}
-}
