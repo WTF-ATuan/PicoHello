685bcdddac6af1b85e2c85b3e7b299023d5c2e3a
Atuan <jain60501@gmail.com.tw>
2022-05-10 11:57:53 +0800

    added function "ModifyRigTotalLength" auto set control index by lenght

--- Assets/HelloPico2/InteractableObjects/Scripts/LightBeamRigController.cs
+++ Assets/HelloPico2/InteractableObjects/Scripts/LightBeamRigController.cs
@@ -1,54 +1,54 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace HelloPico2.InteractableObjects{
 	public class LightBeamRigController : MonoBehaviour{
 		[SerializeField] [Required] private Transform rigRoot;
+		[SerializeField] [MinValue(0.1f)] private float maxRigDistance = 0.25f;
 		private List<Transform> _rigs;
-		private int _currentControlIndex = 5;
-
-		public bool IsLenghtUpdating{ get; private set; }
+		[SerializeField] private int currentControlIndex = 5;
 
 		private void Start(){
 			_rigs = rigRoot.GetComponentsInChildren<Transform>().ToList();
 			_rigs.RemoveAt(0);
 		}
 
-		public float GetAllRigLenght(){
-			float lenght = 0;
-			for(var i = 0; i < _currentControlIndex; i++){
-				var rig = _rigs[i];
-				var localPosition = rig.transform.localPosition;
-				lenght += localPosition.z;
-			}
-
-			return lenght;
-		}
-
-		public float GetSingleRigLenght(){
-			var first = _rigs.First();
-			return first.position.z;
+		private LightBeamLenghtUpdated GetDefaultUpdatedEvent(){
+			var lenghtUpdated = new LightBeamLenghtUpdated();
+			return lenghtUpdated;
 		}
 
 		[Button]
-		public void ModifyPositionLenght(float multiplyValue){
-			for(var i = 0; i < _currentControlIndex; i++){
+		public void ModifyControlRigLenght(float rigLenght){
+			for(var i = 0; i < currentControlIndex; i++){
 				var rig = _rigs[i];
 				var rigTransform = rig.transform;
-				var addPosition = rigTransform.InverseTransformVector(rigTransform.forward * multiplyValue);
+				var addPosition = rigTransform.InverseTransformVector(rigTransform.forward * rigLenght);
 				var finalPosition = rigTransform.localPosition + addPosition;
-				IsLenghtUpdating = true;
 				rigTransform.localPosition = finalPosition;
 			}
+		}
 
-			IsLenghtUpdating = false;
+		[Button]
+		public void ModifyRigTotalLength(float totalLength){
+			var totalOffset = rigRoot.forward * totalLength;
+			var rigCount = Mathf.FloorToInt(totalOffset.z / maxRigDistance);
+			var addedOffset = totalOffset / rigCount;
+			for(var i = 0; i < rigCount; i++){
+				var rig = _rigs[i];
+				var rigTransform = rig.transform;
+				var finalPosition = rigTransform.InverseTransformVector(addedOffset);
+				rigTransform.localPosition = finalPosition;
+			}
 		}
 
 		public void SetPositionLenghtByPercent(float multiplyValue, float value){
-			for(var i = 0; i < _currentControlIndex; i++){
+			for(var i = 0; i < currentControlIndex; i++){
 				var rig = _rigs[i];
 				var rigTransform = rig.transform;
 				var targetPosition = rigTransform.InverseTransformVector(rigTransform.forward * multiplyValue);
@@ -56,5 +56,11 @@ namespace HelloPico2.InteractableObjects{
 				rigTransform.localPosition = lerpPosition;
 			}
 		}
+	}
+
+	public class LightBeamLenghtUpdated{
+		public float TotalLenght;
+		public float SingleLenght;
+		public bool IsUpdating;
 	}
 }
