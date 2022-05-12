6283c99d96e3c124ba90a83997a58fd7509ac907
jeremyhuang <jeremyimage@gmail.com>
2022-05-12 16:54:18 +0800

    fixeDemoHand

--- Assets/HelloPico2/Game Loop/Intro/scripts/moveScript.cs
+++ Assets/HelloPico2/Game Loop/Intro/scripts/moveScript.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class moveScript : MonoBehaviour
 {
-    public GameObject targetOb;
-    float speed = 0.01f;
+    //public GameObject targetOb;
+    public float speed = 0.01f;
     float timer=0;
+    public float desTime=10.0f;
+    public int moveAxis;//xyz
     
     // Update is called once per frame
     private void Update()
     {
         timer += Time.deltaTime;
         Vector3 yMove = transform.up;
-        transform.position += yMove * timer*speed;
-        if (timer > 5f)
+        Vector3 xMove = transform.right;
+        Vector3 zMove = -transform.forward;
+        Vector3 negyMove = -transform.up;
+        if (moveAxis == 0)
+        {
+            transform.position += xMove * timer * speed;
+        }
+        else if(moveAxis == 1)
+        {
+            transform.position += yMove * timer * speed;
+        }
+        else if(moveAxis ==2)
+        {
+            transform.position += zMove * timer * speed;
+        }
+        else if (moveAxis == 3)
+        {
+            
+            transform.position += negyMove * timer * speed;
+        }
+        if (timer > desTime && desTime !=0)
         {
             Destroy(gameObject);
         }
