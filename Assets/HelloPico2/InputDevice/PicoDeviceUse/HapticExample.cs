65949a65c2d45334a5756ca2741603ab2a6fd0ee
Atuan <jain60501@gmail.com.tw>
2022-05-12 15:48:29 +0800

    added Example of Haptic of Pico

---
+++ Assets/HelloPico2/InputDevice/PicoDeviceUse/HapticExample.cs
@@ -0,0 +1,23 @@
+﻿using HelloPico2.InputDevice.Scripts;
+using Project;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace HelloPico2.InputDevice.PicoDeviceUse{
+	public class HapticExample : MonoBehaviour{
+		private ISelector _selector;
+
+		private void Start(){
+			EventBus.Subscribe<DeviceInputDetected>(OnDeviceInputDetected);
+		}
+
+		private void OnDeviceInputDetected(DeviceInputDetected obj){
+			_selector = obj.Selector;
+		}
+
+		[Button]
+		public void Haptic(){
+			_selector.SetHaptic(10, 1);
+		}
+	}
+}
