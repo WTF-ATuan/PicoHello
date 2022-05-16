6016833be99a00a22fa04c094e46c4feea3a0cac
Atuan <jain60501@gmail.com.tw>
2022-05-16 17:19:35 +0800

    added interact feature with collide

---
+++ Assets/HelloPico2/InteractableObjects/Scripts/LightBeam/BeamCollidedAnimation.cs
@@ -0,0 +1,18 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace HelloPico2.InteractableObjects{
+	[RequireComponent(typeof(Animator))]
+	public class BeamCollidedAnimation : MonoBehaviour, IBeamCollide{
+		private Animator _animator;
+		[Required] [SerializeField] private AnimationClip animationClip;
+
+		private void Start(){
+			_animator = GetComponent<Animator>();
+		}
+
+		public void OnCollide(){
+			_animator.Play(animationClip.name);
+		}
+	}
+}
---
+++ Assets/HelloPico2/InteractableObjects/Scripts/LightBeam/BeamCollidedHaptic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HelloPico2.InputDevice.Scripts;
+using Unity.XR.PXR;
+using UnityEngine;
+
+namespace HelloPico2.InteractableObjects{
+	public class BeamCollidedHaptic : MonoBehaviour, IBeamCollide{
+		public float strength;
+		public int time;
+		public HandType type;
+
+
+		public void OnCollide(){
+			switch(type){
+				case HandType.Left:
+					PXR_Input.SetControllerVibration(strength, time, PXR_Input.Controller.LeftController);
+					break;
+				case HandType.Right:
+					PXR_Input.SetControllerVibration(strength, time, PXR_Input.Controller.RightController);
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+	}
+}
