8727c8bb7b52f69893b0003d4853e35e6e155cea
Atuan <jain60501@gmail.com.tw>
2022-05-25 16:30:34 +0800

    added audio Event and AudioEventHandler.cs

--- Assets/HelloPico2/EventSystem/ViewEventDataOverview.cs
+++ Assets/HelloPico2/EventSystem/ViewEventDataOverview.cs
@@ -17,6 +17,10 @@ namespace HelloPico2{
 		[SerializeReference] [PropertyOrder(100)]
 		private List<ViewEventData> viewEventDataList = new List<ViewEventData>();
 
+		public T FindEventData<T>(string id) where T : ViewEventData{
+			var viewEventData = viewEventDataList.Find(x => x.identity.Equals(id));
+			return viewEventData as T;
+		}
 
 		// (使用者) 傳送 我需要聲音事件 (音效ID) => Handler 接收事件 來 OverView 當中找聲音事件 => Handler 幫忙撥出音效 or 給另一個角色播音效。
 	}
