ba7cd58e198661c60201902ba50c68c227ea7156
Atuan <jain60501@gmail.com.tw>
2022-05-30 12:16:22 +0800

    fixed moduleModel

--- Assets/HelloPico2/EventSystem/Scripts/EventData/ModuleModel.cs
+++ Assets/HelloPico2/EventSystem/Scripts/EventData/ModuleModel.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace HelloPico2{
 	[System.Serializable]
 	public class ModuleModel : ViewEventData{
-		[Required] public GameObject modelRoot;
+		[Required] [OnValueChanged("OnModelRootChange")]
+		public GameObject modelRoot;
+
 		public List<GameObject> modelComponent = new List<GameObject>();
+
+		private void OnModelRootChange(){
+			if(!modelRoot) return;
+		}
 	}
 }
