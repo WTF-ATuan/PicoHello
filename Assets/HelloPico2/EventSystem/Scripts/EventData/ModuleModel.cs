834897f225ac2a2ffa1486906c57782dfb0ec05c
Atuan <jain60501@gmail.com.tw>
2022-06-01 18:01:46 +0800

    added child wrapper

--- Assets/HelloPico2/EventSystem/Scripts/EventData/ModuleModel.cs
+++ Assets/HelloPico2/EventSystem/Scripts/EventData/ModuleModel.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using AV.Inspector.Runtime;
 using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor.Drawers;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace HelloPico2{
-	#if UNITY_EDITOR
-	#endif
 	[Serializable]
 	public class ModuleModel : ViewEventData{
-		[Required] [OnValueChanged("OnModelRootChange")]
+		[Required] [OnValueChanged("OnModelRootChange")] [PreviewField]
 		public GameObject modelRoot;
 
-		public List<GameObject> modelComponent = new List<GameObject>();
+		public List<ChildWrapper> modelComponent = new List<ChildWrapper>();
 
+		#if UNITY_EDITOR
 		private void OnModelRootChange(){
 			if(!modelRoot){
 				modelComponent.Clear();
@@ -34,9 +36,32 @@ namespace HelloPico2{
 			foreach(Transform child in obj.transform){
 				if(null == child || !child.gameObject.activeSelf)
 					continue;
-				modelComponent.Add(child.gameObject);
+				var childGameObject = child.gameObject;
+				var childWrapper = new ChildWrapper{
+					value = childGameObject,
+					parent = child.parent,
+					active = childGameObject.activeSelf
+				};
+				modelComponent.Add(childWrapper);
 				GetChildRecursive(child.gameObject);
 			}
+		}
+
+		#endif
+	}
+
+	[Serializable]
+	public class ChildWrapper{
+		[HideLabel] [HorizontalGroup("1", LabelWidth = 20f, Width = 0.1f)]
+		public bool active;
+
+		[GUIColor("GetButtonColor")] [HorizontalGroup("1", LabelWidth = 40f)]
+		public GameObject value;
+
+		[HideInInspector] public Transform parent;
+
+		private Color GetButtonColor(){
+			return active ? Color.white : new Color(0.4f, 0.4f, 0.4f, 1);
 		}
 	}
 }
