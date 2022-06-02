cee846733fe371af9bf851d442c770808d77d28b
Atuan <jain60501@gmail.com.tw>
2022-06-02 14:25:43 +0800

    gitflow-feature-stash: module-Model-Event

---
+++ Assets/HelloPico2/EventSystem/Scripts/ModuleEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Project;
+using UnityEngine;
+
+namespace HelloPico2{
+	public class ModuleEventHandler : MonoBehaviour{
+		[SerializeField] private ViewEventDataOverview overview;
+
+		private void Start(){
+			EventBus.Subscribe<ModuleModelEventRequested>(OnModuleEventRequested);
+		}
+
+		private void OnModuleEventRequested(ModuleModelEventRequested obj){
+			var modelID = obj.ModelID;
+			var moduleModel = overview.FindEventData<ModuleModel>(modelID);
+			var position = obj.Position;
+			var modelRoot = moduleModel.modelRoot;
+			var childWrappers = moduleModel.modelComponent;
+			SpawnModel(modelRoot, childWrappers, position);
+		}
+
+		private void SpawnModel(GameObject root, List<ChildWrapper> childList, Vector3 position){
+					
+			
+			
+			
+		}
+	}
+}
