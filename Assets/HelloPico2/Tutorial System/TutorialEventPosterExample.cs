74ea8b2bd7874112cda3a2275e0cdfc23193c053
Atuan <jain60501@gmail.com.tw>
2022-06-15 17:15:36 +0800

    added behavior Data and TrackedData
    Signed-off-by: Atuan <jain60501@gmail.com.tw>

--- Assets/HelloPico2/Tutorial System/TutorialEventPosterExample.cs
+++ Assets/HelloPico2/Tutorial System/TutorialEventPosterExample.cs
@@ -13,12 +13,22 @@ namespace HelloPico2.TutorialSystem{
 		private Component detectComponent;
 
 		private void Start(){
-			var tryGetComponent = TryGetComponent(typeof(XRBaseInteractable), out var component);
+			var tryGetComponent = TryGetComponent(typeof(XRBaseInteractable), out _);
 			if(!tryGetComponent) throw new Exception($"no Interactable component in {gameObject}");
 			EventBus.Subscribe<DeviceInputDetected>(OnDeviceInputDetected);
 		}
 
-		private void OnDeviceInputDetected(DeviceInputDetected obj){ }
+		private void OnDeviceInputDetected(DeviceInputDetected obj){
+			var isSameObject = obj.IsSameObject(gameObject);
+			if(!isSameObject) return;
+			var trackedData = new BehaviorTrackedData{
+				IsGrip = obj.IsGrip,
+				IsTrigger = obj.IsTrigger,
+				TouchPadAxis = obj.TouchPadAxis
+			};
+			var behaviorDetected = new TutorialBehaviorDetected(detectComponent.GetType(), trackedData);
+			EventBus.Post(behaviorDetected);
+		}
 
 		private IEnumerable GetAllComponent(){
 			var components = GetComponents(typeof(Component)).ToList();
