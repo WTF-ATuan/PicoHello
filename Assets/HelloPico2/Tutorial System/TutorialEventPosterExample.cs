3cc5a448d5477add5eda87de360e33ba7d4bf471
Atuan <jain60501@gmail.com.tw>
2022-06-15 16:57:06 +0800

    remove wrong target scripts
    Signed-off-by: Atuan <jain60501@gmail.com.tw>

--- Assets/HelloPico2/Tutorial System/TutorialEventPosterExample.cs
+++ Assets/HelloPico2/Tutorial System/TutorialEventPosterExample.cs
@@ -1,18 +1,28 @@
-﻿using HelloPico2.InputDevice.Scripts;
+﻿using System;
+using System.Collections;
+using System.Linq;
+using HelloPico2.InputDevice.Scripts;
 using Project;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace HelloPico2.TutorialSystem{
 	public class TutorialEventPosterExample : MonoBehaviour{
+		[ValueDropdown("GetAllComponent")] [SerializeField]
+		private Component detectComponent;
+
 		private void Start(){
+			var tryGetComponent = TryGetComponent(typeof(XRBaseInteractable), out var component);
+			if(!tryGetComponent) throw new Exception($"no Interactable component in {gameObject}");
 			EventBus.Subscribe<DeviceInputDetected>(OnDeviceInputDetected);
 		}
 
-		private void OnDeviceInputDetected(DeviceInputDetected obj){
-			var selectableObject = obj.Selector.SelectableObject;
-			SandBehaviorRequest(selectableObject);
-		}
+		private void OnDeviceInputDetected(DeviceInputDetected obj){ }
 
-		private void SandBehaviorRequest(GameObject obj){ }
+		private IEnumerable GetAllComponent(){
+			var components = GetComponents(typeof(Component)).ToList();
+			return components.Select(x => new ValueDropdownItem(x.GetType().Name, x));
+		}
 	}
 }
