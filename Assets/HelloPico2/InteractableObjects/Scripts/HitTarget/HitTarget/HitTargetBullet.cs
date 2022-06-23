1cac2ab0cf88ca91f453ce24141262b6dd5807b7
Andy <andy@storm8.com>
2022-06-22 23:45:28 -0700

    Organized interactables folder

--- Assets/HelloPico2/InteractableObjects/Scripts/HitTarget/HitTarget/HitTargetBullet.cs
+++ Assets/HelloPico2/InteractableObjects/Scripts/HitTarget/HitTarget/HitTargetBullet.cs
@@ -6,28 +6,34 @@ namespace HelloPico2.InteractableObjects
 {
     public class HitTargetBullet : HitTargetBase
     {
+        [SerializeField] private float _Lifetime = 90f;        
         [SerializeField] private float _DestroyDelayDuration = 3;        
         [SerializeField] private string _HitEffectID = "";
+        private void OnEnable()
+        {
+            Destroy(gameObject, _Lifetime);
+        }
         public override void OnCollide(InteractType type, Collider selfCollider)
         {
             //print("Collide");
+            DestroyBullet(selfCollider);
             WhenCollide?.Invoke();
             base.OnCollide(type, selfCollider);
         }
-        private void OnEnable()
-        {
-            OnEnergyBallInteract += DestroyBullet;
-            OnShieldInteract += DestroyBullet;
-            OnWhipInteract += DestroyBullet;
-            OnBeamInteract += DestroyBullet;
-        }
-        private void OnDisable()
-        {
-            OnEnergyBallInteract -= DestroyBullet;
-            OnShieldInteract -= DestroyBullet;
-            OnWhipInteract -= DestroyBullet;
-            OnBeamInteract -= DestroyBullet;
-        }
+        //private void OnEnable()
+        //{
+        //    OnEnergyBallInteract += DestroyBullet;
+        //    OnShieldInteract += DestroyBullet;
+        //    OnWhipInteract += DestroyBullet;
+        //    OnBeamInteract += DestroyBullet;
+        //}
+        //private void OnDisable()
+        //{
+        //    OnEnergyBallInteract -= DestroyBullet;
+        //    OnShieldInteract -= DestroyBullet;
+        //    OnWhipInteract -= DestroyBullet;
+        //    OnBeamInteract -= DestroyBullet;
+        //}
         private void DestroyBullet(Collider selfCollider) {            
             Project.EventBus.Post<VFXEventRequested, ParticleSystem>(new VFXEventRequested(
                 _HitEffectID, 
