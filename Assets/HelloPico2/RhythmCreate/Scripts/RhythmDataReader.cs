d9b10a551b22097b291f916fa3ab38a48f75026a
Atuan <jain60501@gmail.com.tw>
2022-06-28 16:06:20 +0800

    feat DataReader to spilt string Data
    Signed-off-by: Atuan <jain60501@gmail.com.tw>

--- Assets/HelloPico2/RhythmCreate/Scripts/RhythmDataReader.cs
+++ Assets/HelloPico2/RhythmCreate/Scripts/RhythmDataReader.cs
@@ -1,28 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Melanchall.DryWetMidi.MusicTheory;
 using UnityEngine;
 
 namespace HelloPico2.RhythmCreate.Scripts{
 	public class RhythmDataReader{
-		public readonly Dictionary<NoteName, List<double>> StampDictionary;
+		public readonly Dictionary<NoteName, List<double>> StampDictionary = new Dictionary<NoteName, List<double>>();
 
 		public RhythmDataReader(TextAsset textAsset){
-			StampDictionary = new Dictionary<NoteName, List<double>>();
 			var originalText = textAsset.text;
 			ReadTextData(originalText);
 		}
 
 		private void ReadTextData(string original){
-			var dataSplit = original.Split('\n');
-			foreach(var data in dataSplit){
-				var timeStamps = double.Parse(data); //Bug
-				var containsKey = StampDictionary.ContainsKey(NoteName.C);
-				if(containsKey){
-					StampDictionary[NoteName.C].Add(timeStamps);
+			var lines = original.Split(new[]{ Environment.NewLine }, StringSplitOptions.None);
+			var lineLength = lines.Length / 2;
+			for(var i = 0; i < lineLength; i += 2){
+				var noteName = lines[i];
+				Enum.TryParse<NoteName>(noteName, out var note);
+				var timeStampList = new List<double>();
+				for(var j = 1; j < lineLength; j += 2){
+					var timeStampData = lines[j].Split(',');
+					timeStampList.AddRange(timeStampData.Select(double.Parse));
 				}
-				else{
-					StampDictionary.Add(NoteName.C, new List<double>{ timeStamps });
-				}
+
+				StampDictionary.Add(note, timeStampList);
 			}
 		}
 	}
