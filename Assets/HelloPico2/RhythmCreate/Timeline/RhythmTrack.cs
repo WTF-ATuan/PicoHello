7444e61e1eaca6199687dfd813d7240dd2a6e0cd
Atuan <jain60501@gmail.com.tw>
2022-07-04 15:24:43 +0800

    gitflow-feature-stash: Level_Timeline_Track

--- Assets/HelloPico2/RhythmCreate/Timeline/RhythmTrack.cs
+++ Assets/HelloPico2/RhythmCreate/Timeline/RhythmTrack.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using HelloPico2.RhythmCreate.Scripts;
+using Sirenix.OdinInspector;
+using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
@@ -6,17 +8,33 @@ using UnityEngine.Timeline;
 [TrackClipType(typeof(RhythmClip))]
 public class RhythmTrack : TrackAsset{
 	private TextAsset _textAsset;
+	private RhythmDataReader _dataReader;
+	
+	private void CreateAllNote(){
+		if(!_textAsset) return;
+		_dataReader = new RhythmDataReader(_textAsset);
+		foreach(var clip in GetClips()){
+			DeleteClip(clip);
+		}
 
-	private void OnEnable(){ }
-
+		foreach(var note in _dataReader.StampDictionary){
+			var noteKey = note.Key;
+			var timeList = note.Value;
+			foreach(var timeStamp in timeList){
+				var timelineClip = CreateClip<RhythmClip>();
+				timelineClip.displayName = noteKey.ToString();
+				timelineClip.start = timeStamp;
+				timelineClip.duration = 1f;
+				var rhythmClip = timelineClip.asset as RhythmClip;
+				if(rhythmClip != null)
+					rhythmClip.noteName = noteKey;
+			}
+		}
+	}
 	public override void GatherProperties(PlayableDirector director, IPropertyCollector driver){
 		var binding = director.GetGenericBinding(this);
 		_textAsset = binding as TextAsset;
+		CreateAllNote();
 		base.GatherProperties(director, driver);
-	}
-
-	protected override void OnCreateClip(TimelineClip clip){
-		clip.displayName = "Note";
-		base.OnCreateClip(clip);
 	}
 }
