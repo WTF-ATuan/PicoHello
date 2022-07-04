8cea38944a94683919a1ad2b4d542fce76c89254
Atuan <jain60501@gmail.com.tw>
2022-07-04 14:57:09 +0800

    added all timeline need track
    Signed-off-by: Atuan <jain60501@gmail.com.tw>

---
+++ Assets/HelloPico2/RhythmCreate/Timeline/RhythmClip.cs
@@ -0,0 +1,14 @@
+﻿using Melanchall.DryWetMidi.MusicTheory;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class RhythmClip : PlayableAsset{
+	public NoteName noteName;
+
+	public override Playable CreatePlayable(PlayableGraph graph, GameObject owner){
+		var playable = ScriptPlayable<RhythmBehaviour>.Create(graph);
+		var behaviour = playable.GetBehaviour();
+		behaviour.NoteName = noteName;
+		return playable;
+	}
+}
---
+++ Assets/HelloPico2/RhythmCreate/Timeline/RhythmTrack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[TrackBindingType(typeof(TextAsset))]
+[TrackClipType(typeof(RhythmClip))]
+public class RhythmTrack : TrackAsset{
+	private TextAsset _textAsset;
+
+	private void OnEnable(){ }
+
+	public override void GatherProperties(PlayableDirector director, IPropertyCollector driver){
+		var binding = director.GetGenericBinding(this);
+		_textAsset = binding as TextAsset;
+		base.GatherProperties(director, driver);
+	}
+
+	protected override void OnCreateClip(TimelineClip clip){
+		clip.displayName = "Note";
+		base.OnCreateClip(clip);
+	}
+}
