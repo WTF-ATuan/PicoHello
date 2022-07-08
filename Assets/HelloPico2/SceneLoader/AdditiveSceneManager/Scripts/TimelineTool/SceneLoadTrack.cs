f3eb23c0326809cb7d3955eb0afb98ff4f4de45e
Atuan <jain60501@gmail.com.tw>
2022-07-08 13:33:25 +0800

    feat timelimetool of scene Loader

---
+++ Assets/HelloPico2/SceneLoader/AdditiveSceneManager/Scripts/TimelineTool/SceneLoadTrack.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Timeline;
+
+[TrackBindingType(typeof(SceneController))]
+[TrackClipType(typeof(ScenePlayableAsset))]
+[TrackColor(1, 0, 0)]
+public class SceneLoadTrack : TrackAsset{
+	protected override void OnCreateClip(TimelineClip clip){
+		clip.displayName = "Load Scene";
+		base.OnCreateClip(clip);
+	}
+}
---
+++ Assets/HelloPico2/SceneLoader/AdditiveSceneManager/Scripts/TimelineTool/ScenePlayableBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using HelloPico2.SceneLoader.AdditiveSceneManager.Scripts.MultiScene;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ScenePlayableBehavior : PlayableBehaviour{
+	public SceneObject SceneObject;
+	public LoadOptions LoadOptions;
+
+	private bool _invokeFlag;
+
+	public override void ProcessFrame(Playable playable, FrameData info, object playerData){
+		if(_invokeFlag || !Application.isPlaying) return;
+		if(SceneObject == null) throw new Exception("Scene is None Please Assign");
+		var sceneController = playerData as SceneController;
+		InvokeSceneAction(sceneController);
+	}
+
+	private void InvokeSceneAction(SceneController controller){
+		var sceneName = SceneObject.ToString();
+		switch(LoadOptions){
+			case LoadOptions.Load:
+				controller.LoadScene(sceneName);
+				break;
+			case LoadOptions.BackgroundLoad:
+				controller.BackGroundLoadScene(sceneName);
+				break;
+			case LoadOptions.ActiveBackground:
+				controller.ActiveBackgroundScene(sceneName);
+				break;
+			case LoadOptions.UnLoad:
+				controller.UnloadScene(sceneName);
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+
+		_invokeFlag = true;
+	}
+
+	public override void OnGraphStart(Playable playable){
+		_invokeFlag = false;
+		base.OnGraphStart(playable);
+	}
+
+	public override void OnGraphStop(Playable playable){
+		_invokeFlag = false;
+		base.OnGraphStop(playable);
+	}
+}
