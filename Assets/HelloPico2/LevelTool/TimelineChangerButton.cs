2e9f746a75e11ceacdd402738424ca3692fca393
Atuan <jain60501@gmail.com.tw>
2022-07-15 14:44:31 +0800

    feat developer board can change timeline

---
+++ Assets/HelloPico2/LevelTool/TimelineChangerButton.cs
@@ -0,0 +1,27 @@
+﻿using HelloPico2.SceneLoader.AdditiveSceneManager.Scripts.MultiScene;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HelloPico2.LevelTool{
+	[RequireComponent(typeof(Button))]
+	public class TimelineChangerButton : MonoBehaviour{
+		[Required] public CrossSceneEventPoster eventPoster;
+		private Button _button;
+		private string _changedName;
+
+		private void Start(){
+			_button = GetComponent<Button>();
+			_changedName = gameObject.name;
+			_button.onClick.AddListener(OnButtonClick);
+		}
+
+		[Button]
+		private void OnButtonClick(){
+			var timelineChanged = new TimelineChanged{
+				eventID = _changedName
+			};
+			eventPoster.Post(timelineChanged);
+		}
+	}
+}
