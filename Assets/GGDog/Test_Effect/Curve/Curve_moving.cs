1c70ce914ec0df9724b6ce37d58ad09a620926c2
jeremyhuang <jeremyimage@gmail.com>
2022-07-29 13:20:22 +0800

    update curveTest

---
+++ Assets/GGDog/Test_Effect/Curve/Curve_moving.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curve_moving : MonoBehaviour
+{
+    public float Speed = 50;
+    public AnimationCurve SpeedCurve;
+
+
+
+    float avSpeed;
+    float ori_d;
+
+    GameObject Player_Hand_Pos;
+
+    void OnEnable()
+    {
+        Player_Hand_Pos = GameObject.Find("Player_Hand_Pos");
+
+        ori_d = Vector3.Distance(transform.position, Player_Hand_Pos.transform.position);
+    }
+
+    void Update()
+    {
+        float d = Vector3.Distance(transform.position, Player_Hand_Pos.transform.position);
+
+        avSpeed = Speed * ori_d / d;
+        transform.position = Vector3.Lerp(transform.position, Player_Hand_Pos.transform.position, avSpeed * Time.deltaTime * SpeedCurve.Evaluate(1- d/ori_d ) );
+
+    }
+}
