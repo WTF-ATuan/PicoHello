6aae345d744884d89690f1738f49bc8df160800c
Andy <andy@storm8.com>
2022-07-28 23:50:14 -0700

    Feature - Charged projectile timed explosion

---
+++ Assets/HelloPico2/PlayerController/Arm/Scripts/Projectile/ExplosiveProjectile.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using Sirenix.OdinInspector;
+using HelloPico2.InteractableObjects;
+using UnityEngine;
+
+namespace HelloPico2.PlayerController.Arm
+{
+    public class ExplosiveProjectile : ProjectileController
+    {
+        [SerializeField] private float _CountDownDuration = 1;
+        [SerializeField] private float _DealDamageRange = 5;
+        [SerializeField] private LayerMask _DamageDetectionLayer;
+        [SerializeField] private ParticleSystem _ExplosionVFX;
+        
+        [FoldoutGroup("SFX Settings")][SerializeField] private string _ExplosionSFXName;
+
+        Coroutine timer;
+
+        private void OnEnable()
+        {
+            timer = StartCoroutine(Timer());
+        }
+        private IEnumerator Timer() { 
+            yield return new WaitForSeconds(_CountDownDuration);
+            CheckSurroundedTarget();
+            PlayExplosionEffect();
+        }
+        protected override void OnTriggerEnter(Collider other)
+        {
+            if (CheckSurroundedTarget())
+            {                
+                PlayExplosionEffect();
+                StopCoroutine(timer);
+            }
+        }
+        private bool CheckSurroundedTarget() {
+            var hitInfo = Physics.OverlapSphere(transform.position, _DealDamageRange, _DamageDetectionLayer);
+
+            if (hitInfo.Length > 0)
+            {
+                for (int i = 0; i < hitInfo.Length; i++)
+                {
+                    if (hitInfo[i].TryGetComponent<IInteractCollide>(out var interact))
+                    {
+                        interact.OnCollide(_InteractType, GetComponent<Collider>());
+                    }
+                }
+
+                return true;
+            }
+            else
+                return false;
+        }
+        private void PlayExplosionEffect() {
+            _ExplosionVFX.Play();
+            GetComponent<Collider>().enabled = false;
+            GetComponent<MeshRenderer>().enabled = false;
+            _rigidbody.velocity = Vector3.zero;
+            
+            Project.EventBus.Post(new AudioEventRequested(_ExplosionSFXName, transform.position));
+            WhenCollide?.Invoke();
+
+            Destroy(gameObject, _CollideDestryDelayTime);
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _DealDamageRange);
+        }
+    }
+}
+
