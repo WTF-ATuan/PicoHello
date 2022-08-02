f96cdb1413e60ef7dd7377517ad3c908a0d3a116
Atuan <jain60501@gmail.com.tw>
2022-08-02 15:18:18 +0800

    added event ratingInputRequested

---
+++ Assets/HelloPico2/Rating System/RatingExecutor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Project;
+using UnityEngine;
+
+namespace HelloPico2.Rating_System{
+	public class RatingExecutor : MonoBehaviour{
+		[SerializeField] private RatingSetting setting;
+
+		private BehaviorRating _behaviorRating;
+		private void Start(){
+			EventBus.Subscribe<RatingInputRequested>(OnInputRequested);
+		}
+
+		private void OnInputRequested(RatingInputRequested obj){
+			
+		}
+	}
+}
---
+++ Assets/HelloPico2/Rating System/RatingInputRequested.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace HelloPico2.Rating_System{
+	public class RatingInputRequested{
+		public Transform Origin{ get; }
+		public Transform Target{ get; }
+
+		public RatingInputRequested(Transform origin, Transform target){
+			Origin = origin;
+			Target = target;
+		}
+	}
+}
