438d0bce7936b090e85b949004998a59b977ffc4
jeremyhuang <jeremyimage@gmail.com>
2022-08-02 23:54:07 +0800

    updateLeve:GamePlaySetting

--- Assets/HelloPico2/Game Loop/Menu/Scripts/ObejctPoolScript/ObjPool_Spawner_Jy.cs
+++ Assets/HelloPico2/Game Loop/Menu/Scripts/ObejctPoolScript/ObjPool_Spawner_Jy.cs
@@ -7,17 +7,13 @@ public class ObjPool_Spawner_Jy : MonoBehaviour
     public ObjectPool_Jy pool;
     public float spawnTime = 1f;
     private float _timer;
-    public bool isOne;
+    
     // Update is called once per frame
 
-    private void Start()
-    {
-        SpawnerFunc();
-    }
+
     void Update()
     {
-        if (isOne) return;
-        
+       
         if (Time.time > _timer)
         {
             SpawnerFunc();
