dfd44855392ad81d93c6991dd5e3eba4eb5d0d60
Atuan <jain60501@gmail.com.tw>
2022-08-02 12:40:46 +0800

    feat angle detector to rating player,s behavior

---
+++ Assets/HelloPico2/Rating System/AngleDetector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace HelloPico2.Rating_System{
+	public class AngleDetector : MonoBehaviour{
+		[SerializeField] private Transform origin;
+		[SerializeField] private Transform target;
+
+		[SerializeField] private bool showGizmos = true;
+
+		[SerializeField] [ReadOnly] private float angle;
+		[SerializeField] [ReadOnly] private float offset;
+
+		private Vector3 _trianglePoint;
+
+		private void FixedUpdate(){
+			var targetPosition = target.position;
+			var originPosition = origin.position;
+			var originForward = origin.forward;
+			var direction = targetPosition - originPosition;
+			angle = Vector3.Angle(originForward, direction);
+			var distance = Vector3.Distance(targetPosition, originPosition);
+			_trianglePoint = originPosition + originForward * distance;
+			offset = Vector3.Distance(_trianglePoint, targetPosition);
+		}
+
+		private void OnDrawGizmos(){
+			if(!showGizmos) return;
+			Gizmos.color = Color.red;
+			var originPosition = origin.position;
+			var targetPosition = target.position;
+			Gizmos.DrawLine(originPosition, targetPosition);
+			Gizmos.DrawLine(originPosition, _trianglePoint);
+			Gizmos.DrawLine(_trianglePoint, targetPosition);
+			Gizmos.DrawWireCube(_trianglePoint, Vector3.one);
+		}
+	}
+}
