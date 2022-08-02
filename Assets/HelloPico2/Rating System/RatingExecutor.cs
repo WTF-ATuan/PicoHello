7754ad304f7bd5fa540fb92f8c4313699bd3a45d
Atuan <jain60501@gmail.com.tw>
2022-08-02 16:39:58 +0800

    added player state rating with hitRate

--- Assets/HelloPico2/Rating System/RatingExecutor.cs
+++ Assets/HelloPico2/Rating System/RatingExecutor.cs
@@ -11,6 +11,9 @@ namespace HelloPico2.Rating_System{
 
 		private int _ratingCount;
 		private float _ratingPoint;
+		private float _hitRate;
+
+		[ReadOnly] public string playerState = "Non Rating";
 
 		private void Start(){
 			EventBus.Subscribe<RatingInputRequested>(OnInputRequested);
@@ -30,13 +33,42 @@ namespace HelloPico2.Rating_System{
 			_calculator = new AngleCalculator(origin, target);
 			_behaviorRating = new BehaviorRating(_calculator, setting);
 			CalculateHitRate();
+			CalculateRatingLevel();
 		}
 
 		private void CalculateHitRate(){
 			var offsetRating = _behaviorRating.GetOffsetRating();
 			_ratingPoint += offsetRating;
-			var hitRate = _ratingPoint / _ratingCount;
-			Debug.Log($"{hitRate} = ({_ratingPoint} / {_ratingCount})");
+			_hitRate = _ratingPoint / _ratingCount;
+			Debug.Log($"{_hitRate} = ({_ratingPoint} / {_ratingCount})");
+		}
+
+		private void CalculateRatingLevel(){
+			var ignoreCount = setting.minimumIgnoreCount;
+			if(_ratingCount < ignoreCount) return;
+			var master = _hitRate > setting.masterLevel;
+			var midden = _hitRate > setting.middenLevel;
+			var week = _hitRate > setting.weekLevel;
+			if(master){
+				ChangePlayerState("Master");
+				return;
+			}
+
+			if(midden){
+				ChangePlayerState("Midden");
+				return;
+			}
+
+			if(week){
+				ChangePlayerState("Week");
+				return;
+			}
+		}
+
+		private void ChangePlayerState(string state){
+			if(playerState.Equals(state)) return;
+			Debug.Log($"State Changed to {state} from {playerState}");
+			playerState = state;
 		}
 	}
 }
--- Assets/HelloPico2/Rating System/RatingSetting.cs
+++ Assets/HelloPico2/Rating System/RatingSetting.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace HelloPico2.Rating_System{
 	[CreateAssetMenu(menuName = "HelloPico2/ScriptableObject/ RatingSetting", fileName = "RatingSetting", order = 0)]
 	public class RatingSetting : ScriptableObject{
-		[SerializeField] private List<RatingData> offsetList;
+		[SerializeField] [LabelText("Offset Rating")]
+		private List<RatingData> offsetList;
+
+		[LabelText("Min Ignore")] public int minimumIgnoreCount = 10;
+
+		[BoxGroup("Hit Rate")] [Range(0, 1)] public float weekLevel;
+
+		[FormerlySerializedAs("midLevel")] [BoxGroup("Hit Rate")] [Range(0, 1)] public float middenLevel;
+
+		[FormerlySerializedAs("greatLevel")] [BoxGroup("Hit Rate")] [Range(0, 1)] public float masterLevel;
+
 
 		public List<RatingData> GetOffsetRange(){
 			return offsetList;
@@ -16,6 +27,8 @@ namespace HelloPico2.Rating_System{
 	[Serializable]
 	public class RatingData{
 		[HorizontalGroup] [HideLabel] public float minValue, maxValue;
-		[HorizontalGroup] [HideLabel] public float ratingPoint;
+
+		[HorizontalGroup] [HideLabel] [Range(0, 1)] [MaxValue(1)]
+		public float ratingPoint;
 	}
 }
