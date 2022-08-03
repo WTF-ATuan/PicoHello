0ee1bb7cec04df3fd97dcb313e223b96193c364e
吉吉犬 <102513570+KiraYoshiDog@users.noreply.github.com>
2022-08-04 00:31:49 +0800

    Arm_Flower_injured

--- Assets/GGDog/Formal/Model_Shader/Arm/Arm_Flower/Arm_Flower.cs
+++ Assets/GGDog/Formal/Model_Shader/Arm/Arm_Flower/Arm_Flower.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Arm_Flower : MonoBehaviour
 {
-    [Range(0,1)]
+    [Range(0, 1)]
     public float _h;
+    [Range(0, 1)]
+    public float _injured;
 
     public Material _m1;
     public Material _m2;
@@ -17,18 +19,25 @@ public class Arm_Flower : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        ParticleSystem ps = PS; 
+        ParticleSystem ps = PS;
         var em = ps.emission;
         em.rateOverTime = _h * 200;
 
-        _m1.SetFloat("_h", _h );
-        _m2.SetFloat("_h", (_h - 0.25f ) * (4) );
-        _m3.SetFloat("_h", (_h - 0.50f ) * (4) );
-        _m4.SetFloat("_h", (_h - 0.75f ) * (4) );
+        _m1.SetFloat("_h", _h);
+        _m2.SetFloat("_h", (_h - 0.25f) * (4));
+        _m3.SetFloat("_h", (_h - 0.50f) * (4));
+        _m4.SetFloat("_h", (_h - 0.75f) * (4));
+
+
+
+        _m1.SetFloat("_injured", _injured * 1.5f);
+        _m2.SetFloat("_injured", _injured * 1.5f);
+        _m3.SetFloat("_injured", _injured * 1.5f);
+        _m4.SetFloat("_injured", _injured * 1.5f);
     }
 }
