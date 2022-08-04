643764e0aeea8a8326bc9ff4f7a8cc63116bc19c
吉吉犬 <102513570+KiraYoshiDog@users.noreply.github.com>
2022-08-05 03:13:46 +0800

    Guide Material

---
+++ Assets/GGDog/Formal/Model_Shader/Guide/UVRandomColor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class UVRandomColor : MonoBehaviour
+{
+
+    Material _m;
+    void Start()
+    {
+        _m = GetComponent<SkinnedMeshRenderer>().material;
+
+        _m.SetVector("_RandomOffset", new Vector2((float)Random.Range(0, 5)/5, (float)Random.Range(0, 2)/2));
+
+
+        _m.SetColor("_FadeColor1", new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f),1));
+        _m.SetColor("_FadeColor2", new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1));
+    }
+
+}
