89b89f6d8b57680e0fd60cc56b183114c6f49ba4
jeremyhuang <jeremyimage@gmail.com>
2022-08-04 17:48:51 +0800

    workScene5

---
+++ Assets/HelloPico2/Data/Scripts/UVScrollC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVScrollC : MonoBehaviour
+{
+
+    public float ScrollX = 0.5f;
+    public float ScrollY = 0.5f;
+ 
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offsetX = Time.time * ScrollX;
+        float offsetY = Time.time * ScrollY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
