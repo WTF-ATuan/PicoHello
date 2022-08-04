bb618afca4eb47661b36309a268fd9b0bc5a3626
Atuan <jain60501@gmail.com.tw>
2022-08-04 12:58:50 +0800

    feat Level Fade Timeline tool

---
+++ Assets/HelloPico2/LevelTool/LevelFadePlayableAsset.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class LevelFadePlayableAsset : PlayableAsset{
+	public Level_FadeController.Level level;
+
+	public override Playable CreatePlayable(PlayableGraph graph, GameObject owner){
+		var playable = ScriptPlayable<LevelFadePlayable>.Create(graph);
+		var behaviour = playable.GetBehaviour();
+		behaviour.SelectLevel = level;
+		return playable;
+	}
+}
+
+public class LevelFadePlayable : PlayableBehaviour{
+	public Level_FadeController.Level SelectLevel;
+
+	public override void ProcessFrame(Playable playable, FrameData info, object playerData){
+		var fadeController = (Level_FadeController)playerData;
+		fadeController.level = SelectLevel;
+	}
+}
