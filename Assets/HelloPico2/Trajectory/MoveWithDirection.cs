a2d7d29e289f6a709bdecd6ec2e3799e0e13ab90
Atuan <jain60501@gmail.com.tw>
2022-08-04 21:39:54 +0800

    added Trajectory Fbx And work

---
+++ Assets/HelloPico2/Trajectory/MoveWithDirection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace HelloPico2.Trajectory{
+	public class MoveWithDirection : MonoBehaviour{
+		[SerializeField] private Vector3 direction;
+		[SerializeField] private float speed;
+
+		private void Start(){
+			Destroy(gameObject, 5f);
+		}
+
+		private void Update(){
+			transform.position += direction * speed;
+		}
+	}
+}
---
+++ Assets/HelloPico2/Trajectory/TrajectorySpawner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Game.Project;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace HelloPico2.Trajectory{
+	public class TrajectorySpawner : MonoBehaviour{
+		[FormerlySerializedAs("spawnObject")] [SerializeField]
+		private List<GameObject> spawnObjectList;
+
+		[SerializeField] private int generateCount;
+		[SerializeField] private float randomDuring;
+
+
+		[SerializeField] private List<Transform> tipList;
+
+		private ColdDownTimer _timer;
+
+		private void Start(){
+			GetTipsChildRecursive(gameObject, tipList);
+			_timer = new ColdDownTimer(randomDuring);
+		}
+
+		private void Update(){
+			if(_timer.CanInvoke()){
+				RandomSpawn();
+				_timer.Reset();
+			}
+		}
+
+		private void RandomSpawn(){
+			var randomPointList = tipList.OrderBy(arg => Guid.NewGuid()).Take(generateCount).ToList();
+			var randomObject = spawnObjectList.OrderBy(arg => Guid.NewGuid()).First();
+			foreach(var point in randomPointList){
+				Instantiate(randomObject, point.position, Quaternion.identity);
+			}
+		}
+
+		private void GetTipsChildRecursive(GameObject obj, List<Transform> childList){
+			if(null == obj)
+				return;
+			foreach(Transform child in obj.transform){
+				if(null == child)
+					continue;
+				var childGameObject = child.gameObject;
+				if(childGameObject.name.Contains("Tip")){
+					childList.Add(childGameObject.transform);
+				}
+
+				GetTipsChildRecursive(childGameObject, childList);
+			}
+		}
+	}
+}
