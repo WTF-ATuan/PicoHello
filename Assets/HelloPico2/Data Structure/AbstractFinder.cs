b1fa3092e3dcbc334633ca824105f2d76302f4bd
Atuan <jain60501@gmail.com.tw>
2022-08-08 14:17:24 +0800

    added Data Structure feat and Draw flow on Miro
    https://miro.com/app/board/uXjVOCE-axs=/

---
+++ Assets/HelloPico2/Data Structure/AbstractFinder.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HelloPico2.Data_Structure{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="F"> Finder Type </typeparam>
+	/// <typeparam name="E"> Found Entity Type</typeparam>
+	[SuppressMessage("ReSharper", "InconsistentNaming")]
+	public abstract class AbstractFinder<F, E> where F : new(){
+		public static F NewInstance(){
+			return new F();
+		}
+
+		public abstract E Find();
+	}
+}
---
+++ Assets/HelloPico2/Data Structure/GuildDataFinder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace HelloPico2.Data_Structure{
+	public class GuildDataFinder{
+		
+		public static GuildDataFinder NewInstance(){
+			return new GuildDataFinder();
+		}
+
+		public Animation Find(){
+			// 發出搜索事件 
+			// 誰要接收搜索事件 (接收者必須擁有找尋資料的權限) 擁有權限的角色需要是Mono? 
+			// 依循相應Input 找到對應資料並 通過事件回傳
+			return null;
+		}
+	}
+}
