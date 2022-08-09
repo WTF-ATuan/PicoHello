0ad1cb823e5710b8be827465ad9927ea4c3e1114
Atuan <jain60501@gmail.com.tw>
2022-08-09 15:41:49 +0800

    updated See Through

--- Assets/HelloPico2/Data Structure/DataAdapter.cs
+++ Assets/HelloPico2/Data Structure/DataAdapter.cs
@@ -1,7 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Project;
+using UnityEngine;
 
 namespace HelloPico2.Data_Structure{
 	public class DataAdapter : MonoBehaviour{
-		
+		private void RegisterFindEvent(){
+			EventBus.Subscribe<FindRequested, List<object>>(OnFindRequested);
+		}
+
+		private List<object> OnFindRequested(FindRequested requested){
+			var dataType = requested.RequestDataType;
+			var findMatchData = FindMatchData(dataType);
+			return findMatchData;
+		}
+
+		private List<object> FindMatchData(Type matchType){
+			//Find MatchType Data in overview
+			var matchList = new List<object>();
+			return matchList;
+		}
 	}
 }
