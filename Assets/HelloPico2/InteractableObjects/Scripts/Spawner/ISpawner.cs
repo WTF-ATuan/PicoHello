d7c71049a43524fb5f5706231d3e834619fbf340
Atuan <jain60501@gmail.com.tw>
2022-08-16 15:36:34 +0800

    Revert "feat remove Spawner open API"
    This reverts commit e100db65c6ebb7aa459d25b794d90259dd6021f6.

--- Assets/HelloPico2/InteractableObjects/Scripts/Spawner/ISpawner.cs
+++ Assets/HelloPico2/InteractableObjects/Scripts/Spawner/ISpawner.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 namespace HelloPico2.InteractableObjects{
 	public interface ISpawner{
 		public Action<GameObject> OnSpawn{ get; set; }
+		public List<Vector3> SpawnPoint{ get;  }
 	}
 }
