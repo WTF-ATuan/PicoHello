d6ca5ca7de58f68d16adcffd452bbda00c48ffcf
Atuan <jain60501@gmail.com.tw>
2022-08-17 14:39:57 +0800

    feat simple loop animator to control spawner active and deactive

---
+++ Assets/HelloPico2/InteractableObjects/Scripts/Spawner/SpawnLoopAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace HelloPico2.InteractableObjects{
+	public class SpawnLoopAnimator : MonoBehaviour{
+		[SerializeField] [EnumToggleButtons] private SpawnLoopType loopType;
+
+		[SerializeField] [ShowIf("IsDuringLoopType")]
+		private float during = 5;
+
+		[SerializeField] private float delayOpenTime = 1f;
+		
+		private void OnEnable(){
+			Loop();
+		}
+
+		private async void Loop(){
+			await Task.Delay(Mathf.FloorToInt(during * 1000));
+			gameObject.SetActive(false);
+			await Task.Delay(Mathf.FloorToInt(delayOpenTime * 1000));
+			gameObject.SetActive(true);
+		}
+
+		private bool IsDuringLoopType() => loopType == SpawnLoopType.During;
+	}
+
+	public enum SpawnLoopType{
+		During,
+		FollowSpawner,
+	}
+}
