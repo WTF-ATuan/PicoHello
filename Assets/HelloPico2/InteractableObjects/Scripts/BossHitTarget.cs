e7c73a2b64f88f76787c9b337f5d495fccb92ef0
Atuan <jain60501@gmail.com.tw>
2022-08-24 16:19:17 +0800

    added boss Timer and test in spawner Demo

---
+++ Assets/HelloPico2/InteractableObjects/Scripts/BossHitTarget.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace HelloPico2.InteractableObjects.Scripts{
+	public class BossHitTarget : MonoBehaviour{
+		[ReadOnly] public float elapsedTime = 0;
+		[Required] public ParticleSystem defaultEffect;
+		public List<HitEffect> effectSettings = new List<HitEffect>();
+
+		private ParticleSystem _currentEffect;
+
+
+		private void OnEnable(){
+			ChangeEffect(defaultEffect);
+		}
+
+		private void OnDisable(){
+			elapsedTime = 0;
+			foreach(var effect in effectSettings){
+				effect.isTrigger = false;
+			}
+		}
+
+		private void LateUpdate(){
+			elapsedTime += Time.fixedDeltaTime;
+			foreach(var effect in effectSettings
+							.Where(effect => elapsedTime > effect.triggerTime)
+							.Where(effect => !effect.isTrigger)){
+				ChangeEffect(effect.effect);
+				effect.isTrigger = true;
+			}
+		}
+
+		private void ChangeEffect(ParticleSystem effect){
+			if(_currentEffect){
+				var currentEffectObject = _currentEffect.gameObject;
+				Destroy(currentEffectObject);
+			}
+
+			var currentTransform = transform;
+			var effectObject = Instantiate(effect, currentTransform.position, Quaternion.identity, currentTransform);
+			_currentEffect = effectObject;
+		}
+
+		private void OnTriggerEnter(Collider other){
+			var collisionPoint = other.ClosestPoint(transform.position);
+			_currentEffect.transform.position = collisionPoint;
+			_currentEffect.Play();
+		}
+
+		[Button]
+		private void TestTrigger(Vector3 offset){
+			var triggerPosition = transform.position + offset;
+			_currentEffect.transform.position = triggerPosition;
+			_currentEffect.Play();
+		}
+
+		[Serializable]
+		public class HitEffect{
+			public float triggerTime = 20;
+			[Required] public ParticleSystem effect;
+			public bool isTrigger;
+		}
+	}
+}
