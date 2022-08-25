2939cd90c67028bc1626a9999d873b01c013ed14
Atuan <jain60501@gmail.com.tw>
2022-08-25 15:07:03 +0800

    feat interrupt audio system

---
+++ Assets/HelloPico2/LevelTool/FadeAudio.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using HelloPico2.SceneLoader.AdditiveSceneManager.Scripts.MultiScene;
+using UnityEngine;
+
+namespace HelloPico2.LevelTool{
+	[RequireComponent(typeof(AudioSource))]
+	public class FadeAudio : MonoBehaviour{
+		private AudioSource _audioSource;
+
+		private void Awake(){
+			_audioSource = GetComponent<AudioSource>();
+		}
+
+		public void Handle(CrossEvent crossEvent){
+			var audioInterrupted = (AudioInterrupted)crossEvent;
+			var fadeTime = audioInterrupted.fadeTime;
+			_audioSource.DOFade(0, fadeTime)
+					.OnComplete(() => _audioSource.DOFade(1, 0));
+		}
+	}
+}
