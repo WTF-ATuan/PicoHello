e1df8f89f544bec44ccf227d1db20a7e7b13ad52
jeremyhuang <jeremyimage@gmail.com>
2022-09-01 11:58:25 +0800

    Update IntroTipScale BossSet

---
+++ Assets/HelloPico2/Game Loop/Ch.2/Singnal/setLevel_Env.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setLevel_Env : MonoBehaviour
+{
+    Level_FadeController _level_FadeController;
+    public string setName;
+    public GameObject setLevel;
+    public GameObject hideLevel;
+    public GameObject setTunnelDefault;
+    public bool isfrist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isfrist)
+        {
+            setLevel.GetComponent<Level_FadeController>().Enable_Env = false;
+        }
+        SettingLevelEnv();
+    }
+    private void SettingLevelEnv()
+    {
+        hideLevel.SetActive(false);
+        setTunnelDefault.transform.position = Vector3.zero;
+    }
+
+}
