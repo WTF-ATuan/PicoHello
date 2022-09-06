20f8f6dccdf2fceea5aaf7d2cb5e76dccffdbae8
Atuan <jain60501@gmail.com.tw>
2022-09-06 16:01:50 +0800

    added Shacking Detector but can,t reset

---
+++ Assets/HelloPico2/LevelTool/ShackingDetector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using HelloPico2.InputDevice.Scripts;
+using Project;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace HelloPico2.LevelTool{
+	public class ShackingDetector : MonoBehaviour{
+		[SerializeField] private float shackingSpeed = 2;
+		[SerializeField] private float shackingTime = 2;
+		[SerializeField] private UnityEvent onShackingDetected;
+		
+
+		[BoxGroup("Debug")] [ReadOnly]
+		public float detectedTime = 0;
+
+		private void OnEnable(){
+			EventBus.Subscribe<DeviceInputDetected>(OnInputDetected);
+		}
+
+		private void OnDisable(){
+			detectedTime = 0;
+		}
+
+		private void OnInputDetected(DeviceInputDetected obj){
+			if(!gameObject.activeSelf) return;
+			var selectorSpeed = obj.Selector.Speed;
+			if(selectorSpeed > shackingSpeed){
+				detectedTime += Time.fixedDeltaTime;
+			}
+			Condition();
+		}
+
+		private void Condition(){
+			if(detectedTime < shackingTime) return;
+			onShackingDetected?.Invoke();
+			detectedTime = 0;
+		}
+	}
+}
