b70ffd907f6d5d13cfad985447d1e56312be1c72
吉吉犬 <102513570+KiraYoshiDog@users.noreply.github.com>
2022-09-09 17:33:13 +0800

    水球: 發射變形

--- Assets/HelloPico2/Data/VFX/GGDog/Formal/Effect_Shader/dew/GLOBAL_Pos.cs
+++ Assets/HelloPico2/Data/VFX/GGDog/Formal/Effect_Shader/dew/GLOBAL_Pos.cs
@@ -5,46 +5,23 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class GLOBAL_Pos : MonoBehaviour
 {
-    public float distance;
-
     GameObject[] gos;
 
+    Vector3 followPos;
+
+    private void OnEnable()
+    {
+        followPos = new Vector3(transform.position.x, transform.position.y, 10f);
+    }
     void Update()
     {
-        Vector4 pos = transform.position;
+        Vector3 pos = transform.position;
+
+        followPos = Vector3.Lerp(followPos, transform.position, 0.05f);
+
         Shader.SetGlobalVector("GLOBAL_Pos", pos);
 
-        /*
-        gos = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        Shader.SetGlobalVector("Follow_Pos", followPos);
 
-        Vector4[] gos2 = new Vector4[gos.Length];
-
-        int j = 0;
-        for (int i = 0; i < gos.Length; i++)
-        {
-            gos2[i] = pos;
-            if (gos[i].layer == 8 && Vector3.Distance(gos[i].transform.position, pos)<= distance)
-            {
-                if (gos[i].GetComponent<MeshRenderer>()&&gos[i].GetComponent<MeshRenderer>().sharedMaterial.name == "dew_GPUInstance") 
-                { 
-                    gos2[j] = gos[i].transform.position;
-                    j++;
-                }
-            }
-        }
-        Shader.SetGlobalVectorArray("All_Pos", gos2);*/
-    }
-
-
-    [ContextMenu("[ResetPos]")]
-    void ResetPos()
-    {
-        Vector4[] gos2 = new Vector4[10];
-
-        for (int i = 0; i < 10; i++)
-        {
-            gos2[i] = transform.position;
-        }
-        Shader.SetGlobalVectorArray("All_Pos", gos2);
     }
 }
