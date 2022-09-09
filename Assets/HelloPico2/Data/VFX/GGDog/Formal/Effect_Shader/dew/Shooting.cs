8a724a040dc1c8158efe0c840ef7f8c73ced5295
吉吉犬 <102513570+KiraYoshiDog@users.noreply.github.com>
2022-09-09 17:36:59 +0800

    dew shooting script

--- Assets/HelloPico2/Data/VFX/GGDog/Formal/Effect_Shader/dew/Shooting.cs
+++ Assets/HelloPico2/Data/VFX/GGDog/Formal/Effect_Shader/dew/Shooting.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class GLOBAL_Pos : MonoBehaviour
+public class Shooting : MonoBehaviour
 {
     GameObject[] gos;
 
