64f3272fe8655ce1870b61d6ae857e745e145990
Atuan <jain60501@gmail.com.tw>
2022-09-26 14:49:41 +0800

    remove old scripts of EventTracker and added EventExporter

---
+++ Assets/HelloPico2/Event Tracker/EventExporter.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace Actor.Scripts.EventMessage{
+	public class EventExporter{
+		public string FilePath{ get; }
+		public string FileName{ get; }
+
+		private readonly FileStream _file;
+
+		private string _fileFullName;
+
+		private StreamWriter _streamWriter;
+
+		public EventExporter(string filePath, string fileName){
+			_fileFullName = filePath + "/" + $"{fileName}" + ".json";
+			_file = new FileStream(_fileFullName, FileMode.CreateNew);
+			_streamWriter = new StreamWriter(_file);
+			FilePath = filePath;
+			FileName = fileName;
+		}
+
+
+		public void WriteMessage(){
+			// var jsonString = JsonUtility.ToJson();
+			// _streamWriter.Write(jsonString + Environment.NewLine);
+			_streamWriter.Flush();
+		}
+
+		public void Timeout(){
+			_streamWriter.Flush();
+			_streamWriter.Close();
+			_streamWriter.Dispose();
+		}
+	}
+}
