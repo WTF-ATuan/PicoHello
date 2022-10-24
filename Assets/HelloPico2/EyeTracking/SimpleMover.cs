2b2661d691b00381789885da8bd3e4af9fbbd09a
Atuan <jain60501@gmail.com.tw>
2022-10-24 11:32:36 +0800

    added offset of Simple Mover

--- Assets/HelloPico2/EyeTracking/SimpleMover.cs
+++ Assets/HelloPico2/EyeTracking/SimpleMover.cs
@@ -8,16 +8,20 @@ namespace HelloPico2.EyeTracking{
 		[SerializeField] private float during;
 		[SerializeField] private bool ignoreZ = true;
 		[SerializeField] private AnimationCurve movingCurve = AnimationCurve.Linear(0, 0, 1, 1);
+		public bool useOffset;
+		[ShowIf("useOffset")] public Vector3 offset = Vector3.one;
 
 
 		[Button]
 		public void Move(Vector3 targetPosition){
 			if(ignoreZ){
 				targetPosition.z = controlObject.position.z;
+				if(useOffset) targetPosition += offset;
 				controlObject.DOMove(targetPosition, during)
 						.SetEase(movingCurve);
 			}
 			else{
+				if(useOffset) targetPosition += offset;
 				controlObject.DOMove(targetPosition, during)
 						.SetEase(movingCurve);
 			}
