78d246c51b67da420bb4bd0d7a8c472ca339f301
Andy <andy@storm8.com>
2022-10-25 22:19:12 -0700

    Adding more features to tutorial UI

--- Assets/HelloPico2/LevelTool/LineDrawer.cs
+++ Assets/HelloPico2/LevelTool/LineDrawer.cs
@@ -18,10 +18,12 @@ namespace HelloPico2.LevelTool
         [FoldoutGroup("To Settings")] public ArmControllerInputMeshNameData.InputMesh _ShowOnThisInputMesh; 
         
         public Vector3 _FromOffset;
+        public float _ToOffsetStart = 0.8f;
         public Vector3 _ToOffset;
         public Vector3 _CurveOffset;
         public int _Percision = 100;
         public AnimationCurve _Curve;
+        public AnimationCurve _ToOffsetCurve;
         ArmData armData;
         [ReadOnly][SerializeField] private Transform to;
         List<Vector3> points = new List<Vector3>();
@@ -79,11 +81,18 @@ namespace HelloPico2.LevelTool
 
             points.Clear();
             dist = Vector3.Distance(_From.position + _FromOffset, to.position + _ToOffset);
-
+            var toOffset = _ToOffset;
             for (int i = 0; i < _Percision; i++)
             {
                 var step = (i * dist / _Percision);
-                var Pos = Vector3.Lerp(_From.position + _FromOffset, to.position + _ToOffset, step);
+
+                if (step > _ToOffsetStart)
+                {
+                    var approachingStep = (step - _ToOffsetStart) / (1 - _ToOffsetStart);
+                    toOffset = Vector3.Lerp(_ToOffset, Vector3.zero, _ToOffsetCurve.Evaluate(approachingStep));
+                }
+
+                var Pos = Vector3.Lerp(_From.position + _FromOffset, to.position + toOffset, step);
                 var curve = Vector3.Lerp(Vector3.zero, _CurveOffset, _Curve.Evaluate(step));
                 Pos = Pos + curve;
                 points.Add(Pos);
