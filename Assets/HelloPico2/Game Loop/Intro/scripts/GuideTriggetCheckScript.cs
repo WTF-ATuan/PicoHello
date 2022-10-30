90699ce46834228abf6cb5cc69c3ad4fc94d42bb
jeremyhuang <jeremyimage@gmail.com>
2022-10-30 23:00:30 +0800

    更新-小精靈初始HiFive判斷

--- Assets/HelloPico2/Game Loop/Intro/scripts/GuideTriggetCheckScript.cs
+++ Assets/HelloPico2/Game Loop/Intro/scripts/GuideTriggetCheckScript.cs
@@ -7,14 +7,18 @@ public class GuideTriggetCheckScript : MonoBehaviour
     public GameObject getGuideAnimator;
     public string triggerName;
     Animator guideAnimator;
+    BoxCollider closeCollider;    
+    
     // Start is called before the first frame update
     void Start()
     {
         guideAnimator = getGuideAnimator.GetComponent<Animator>();
+        closeCollider = this.GetComponent<BoxCollider>();
     }
     private void OnTriggerEnter(Collider other)
     {
         guideAnimator.SetTrigger(triggerName);
+        closeCollider.enabled = false;
     }
 
 
