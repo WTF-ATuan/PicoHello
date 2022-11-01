d1c99a383c489d2a3e42ae571e3d95a5e0ee4065
Andy <andy@storm8.com>
2022-11-01 02:42:12 -0700

    Sword, Whip Activate feedback

---
+++ Assets/HelloPico2/PlayerController/Arm/Scripts/IWeaponFeedbacks.cs
@@ -0,0 +1,9 @@
+using HelloPico2.InteractableObjects;
+
+namespace HelloPico2.Interface
+{
+    public interface IWeaponFeedbacks
+    {
+        public void OnSwithWeapon(InteractableSettings.InteractableType interactableType);
+    }
+}
---
+++ Assets/HelloPico2/PlayerController/Arm/Scripts/WeaponItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using HelloPico2.InteractableObjects;
+using HelloPico2.Interface;
+using UnityEngine;
+
+
+public class WeaponItem : MonoBehaviour, IWeaponFeedbacks
+{
+    [System.Serializable]
+    public struct WeaponForm {
+        public InteractableSettings.InteractableType WeaponType;
+        public UltEvents.UltEvent WhenSwitchToWeapon; 
+    }
+    public List<WeaponForm> Weapons = new List<WeaponForm>();
+    public InteractableSettings.InteractableType _EnableFeedback;
+    protected virtual void OnEnable()
+    {
+        OnSwithWeapon(_EnableFeedback);
+    }
+    public virtual void OnSwithWeapon(InteractableSettings.InteractableType interactableType)
+    {
+        for (int i = 0; i < Weapons.Count; i++)
+        {
+            if (interactableType == Weapons[i].WeaponType)
+            { 
+                Weapons[i].WhenSwitchToWeapon?.Invoke();
+                print(Weapons[i].WeaponType);
+            }
+        }
+    }
+}
