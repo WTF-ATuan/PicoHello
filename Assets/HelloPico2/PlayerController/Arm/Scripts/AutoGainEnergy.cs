f1bd6ebf4229b64f43e29b0ade752bb0a610a348
Atuan <jain60501@gmail.com.tw>
2022-11-03 15:07:45 +0800

    feat auto gain Energy test version

---
+++ Assets/HelloPico2/PlayerController/Arm/Scripts/AutoGainEnergy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Game.Project;
+using HelloPico2.InputDevice.Scripts;
+using Project;
+using UnityEngine;
+
+namespace HelloPico2.PlayerController.Arm.Scripts{
+	public class AutoGainEnergy : MonoBehaviour{
+		[SerializeField] private AnimationCurve curve = AnimationCurve.Linear(1, 1, 0, 0);
+		private const float UpdateFrameTime = 0.2f;
+
+		private ArmData _armData;
+		private EnergyBallBehavior _energyBehavior;
+
+
+		private float _timer;
+		private ColdDownTimer _frameTimer;
+
+
+		private void Start(){
+			_armData = GetComponent<ArmData>();
+			_energyBehavior = GetComponent<EnergyBallBehavior>();
+			EventBus.Subscribe<DeviceInputDetected>(OnInputDetected);
+			_frameTimer = new ColdDownTimer(UpdateFrameTime);
+		}
+
+		private void OnInputDetected(DeviceInputDetected obj){
+			if(!_frameTimer.CanInvoke()) return;
+			if(!obj.IsGrip){
+				_timer = 0;
+				return;
+			}
+
+			_timer += Time.deltaTime;
+			CalculateEnergy();
+			_frameTimer.Reset();
+		}
+
+		private void CalculateEnergy(){
+			var lerpTime = Mathf.Lerp(0, 6, _timer);
+			var timeValue = curve.Evaluate(lerpTime);
+			var energy = Mathf.Lerp(0, _armData.MaxEnergy, timeValue);
+			energy += _armData.Energy;
+			_energyBehavior.ChargeEnergyDirectly(energy);
+		}
+	}
+}
