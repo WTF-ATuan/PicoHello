93eef57286819cc25940ee263d0b221bac5cbcd0
jeremyhuang <jeremyimage@gmail.com>
2022-11-07 13:17:56 +0800

    新舊控制器同時顯示Bug修正 教學石頭速度修正

--- Assets/Plugins/EasyEventMaker/Scripts/OtherScript/MoveObject.cs
+++ Assets/Plugins/EasyEventMaker/Scripts/OtherScript/MoveObject.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class MoveObject : MonoBehaviour {
 	public float speed = 20;
 	public Vector3 relativeDirection = Vector3.forward;
-
+    public bool isNotRandomSpeed;
     // Update is called once per frame
     private void Start()
     {
-        speed = Random.Range(1, speed);
+        if (!isNotRandomSpeed)
+        { 
+            speed = Random.Range(1, speed);
+        }
+        
     }
     void Update () {
 		Vector3 absoluteDirection = transform.rotation * relativeDirection;
