c2db6beb2e72cbdfd750067911552fe3f1e3cf91
Andy <andy@storm8.com>
2022-11-14 23:48:55 -0800

    CH4 Gameplay iteration, energyball linking effect, haptic and audio feedbacks

--- Assets/HelloPico2/PlayerController/Arm/Scripts/BeamCharge/LineRendererDrawer.cs
+++ Assets/HelloPico2/PlayerController/Arm/Scripts/BeamCharge/LineRendererDrawer.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,28 @@ public class LineRendererDrawer : MonoBehaviour
     public Transform _To;
     private int positionCount= 2;
     List<Vector3> points = new List<Vector3>();
+
+    [Header("Curve")]
+    public bool _UseCurve;
+    public Vector3 _FromOffset;
+    [Range(0, 1f)] public float _ToOffsetStart = 0.8f;
+    public Vector3 _ToOffset;
+    public Vector3 _CurveOffset;
+    public int _Percision = 100;
+    public AnimationCurve _Curve;
+    public AnimationCurve _ToOffsetCurve;
+    public float _StartFlattenCurveDist;
+
+    float dist;
+
     private void Awake()
     {
-        _LineRenderer.positionCount = positionCount;
+        if (_UseCurve)        
+            _LineRenderer.positionCount = _Percision;        
+        else 
+            _LineRenderer.positionCount = positionCount;            
         
-        for (int i = 0; i < positionCount; i++)
+        for (int i = 0; i < _LineRenderer.positionCount; i++)
         {
             points.Add(Vector3.zero);
         }
@@ -22,9 +40,39 @@ public class LineRendererDrawer : MonoBehaviour
     {
         if (_From == null || _To == null) return;
 
+        if (_UseCurve)
+            DrawCurve();
+        else
+            DrawLine();
+    }
+    private void DrawLine() {
         points[0] = _From.position;
         points[1] = _To.position;
+
+        _LineRenderer.SetPositions(points.ToArray());
+    }
+    private void DrawCurve()
+    {
+        points.Clear();
+        var from = _From.TransformPoint(_FromOffset);
+        dist = Vector3.Distance(from, _To.position + _ToOffset);
         
+        var toOffset = _ToOffset;
+        //print(dist - _StartFlattenCurveDist);
+        var curveOffset = Vector3.Lerp(Vector3.zero, _CurveOffset, Mathf.Clamp(dist - _StartFlattenCurveDist, 0, 1));
+
+        for (int i = 0; i < _Percision; i++)
+        {
+            var step = Mathf.Clamp(i * (dist / _Percision), 0, 1);
+
+            if(i + 1 == _Percision) step = 1;
+
+            var Pos = Vector3.Lerp(from, _To.position + toOffset, step);
+            var curve = Vector3.Lerp(Vector3.zero, curveOffset, _Curve.Evaluate(step));
+            Pos = Pos + curve;
+            points.Add(Pos);
+        }
+
         _LineRenderer.SetPositions(points.ToArray());
     }
 }
