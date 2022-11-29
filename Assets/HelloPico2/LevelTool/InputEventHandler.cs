17b07387fe78d24eb3fee13f31a17f1a2b820995
Atuan <jain60501@gmail.com.tw>
2022-11-29 15:13:10 +0800

    added InputEvent Handler to call input event

---
+++ Assets/HelloPico2/LevelTool/InputEventHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HelloPico2.InputDevice.Scripts;
+using Project;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace HelloPico2.LevelTool{
+	public class InputEventHandler : MonoBehaviour{
+		public List<InputUsages> enableUsages = new List<InputUsages>();
+
+		[BoxGroup] public UnityEvent onUsageTrigger;
+
+		private void Start(){
+			EventBus.Subscribe<DeviceInputDetected>(HandleInput);
+		}
+
+		private bool _isInvoke;
+
+		private void HandleInput(DeviceInputDetected obj){
+			var triggerList = new List<bool>{
+				obj.IsTrigger,
+				obj.IsGrip,
+				obj.IsPrimary,
+				obj.IsSecondary,
+				obj.IsMenu
+			};
+			var containList = new List<bool>{
+				enableUsages.Contains(InputUsages.TriggerButton),
+				enableUsages.Contains(InputUsages.GripButton),
+				enableUsages.Contains(InputUsages.PrimaryButton),
+				enableUsages.Contains(InputUsages.SecondaryButton),
+				enableUsages.Contains(InputUsages.MenuButton),
+			};
+			if(triggerList.Any(x => x == false))
+				_isInvoke = false;
+
+			if(triggerList.SequenceEqual(containList) && !_isInvoke){
+				onUsageTrigger?.Invoke();
+				_isInvoke = true;
+			}
+		}
+	}
+
+	public enum InputUsages{
+		TriggerButton,
+		GripButton,
+		PrimaryButton,
+		SecondaryButton,
+		MenuButton,
+	}
+}
