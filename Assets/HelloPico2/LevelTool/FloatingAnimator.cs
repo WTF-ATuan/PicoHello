cd2931f10a6326a31536492282ebb3920fa3d633
Atuan <jain60501@gmail.com.tw>
2022-12-15 18:15:46 +0800

    added Floating animator to float

---
+++ Assets/HelloPico2/LevelTool/FloatingAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace HelloPico2.LevelTool{
+	public class FloatingAnimator : MonoBehaviour{
+		public float offsetY = 2;
+		public float duration = 1.5f;
+		public bool reset = true;
+
+		private void OnEnable(){
+			var targetOffsetY = transform.position.y + offsetY;
+			transform.DOMoveY(targetOffsetY, duration)
+					.SetLoops(-1, LoopType.Yoyo);
+		}
+
+		private void OnDisable(){
+			if(reset){
+				transform.DORestart();
+			}
+
+			transform.DOKill();
+		}
+	}
+}
