1a2d1ea667f1ecac45b55fb52378f07cf08da830
Atuan <jain60501@gmail.com.tw>
2022-12-20 15:57:00 +0800

    added Select Arm Example

---
+++ Assets/HelloPico2/LevelTool/SelectArmExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace HelloPico2.LevelTool{
+	public class SelectArmExample : MonoBehaviour{
+		public List<GameObject> selectedObjects = new List<GameObject>();
+		public TargetItem_SO itemSo;
+
+		private void Start(){
+			//全部物件關閉
+			selectedObjects.ForEach(x => x.SetActive(false));
+		}
+
+		[Button]
+		private void OpenSelectItem(){
+			var targetItemName = itemSo.targetItemName;
+			var foundObject = selectedObjects.Find(x => targetItemName.Contains(x.name));
+			if(foundObject) foundObject.SetActive(true);
+		}
+	}
+}
