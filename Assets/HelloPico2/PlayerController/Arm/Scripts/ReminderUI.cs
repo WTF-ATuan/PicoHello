971a8b955941bf9ac216ecf36f5aaa7c53adbe66
Andy <andy@storm8.com>
2023-02-06 21:51:03 -0800

    Controller Reminder

--- Assets/HelloPico2/PlayerController/Arm/Scripts/ReminderUI.cs
+++ Assets/HelloPico2/PlayerController/Arm/Scripts/ReminderUI.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using Sirenix.OdinInspector;
 using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 
 namespace HelloPico2.LevelTool
 {
@@ -12,62 +14,72 @@ namespace HelloPico2.LevelTool
         public EnableEvent _HideReminder;
         public EnableEvent _UseSword;
         public EnableEvent _UseShield;
-                
+                        
         public ParticleSystem _UpArrow;
         public ParticleSystem _DownArrow;
+        public ParticleSystem _DuoArrow;
         public ParticleSystem _Whip;
         public ParticleSystem _Shield;
 
+        [SerializeField] private float _IconFadeInDuration = 0.5f;
+        [SerializeField] private float _IconFadeOutDuration = 0.1f;
+
         private Material upArrowMat;
         private Material downArrowMat;
+        private Material duoArrowMat;
         private Material whipMat;
         private Material shieldMat;
-
-        Sequence swordSeq;
-        Sequence shieldSeq;
+        private TweenerCore<float, float, FloatOptions> Sword;
+        private TweenerCore<float, float, FloatOptions> Shield;
+        private TweenerCore<float, float, FloatOptions> DuoArrow;
 
         private void Awake()
         {
-            upArrowMat = _UpArrow.GetComponent<Renderer>().material;
-            downArrowMat = _DownArrow.GetComponent<Renderer>().material;
+            if (_UpArrow != null) upArrowMat = _UpArrow.GetComponent<Renderer>().material;
+            if (_DownArrow != null) downArrowMat = _DownArrow.GetComponent<Renderer>().material;
+            if (_DuoArrow != null) duoArrowMat = _DuoArrow.GetComponent<Renderer>().material;
             whipMat = _Whip.GetComponent<Renderer>().material;
             shieldMat = _Shield.GetComponent<Renderer>().material;
 
-            swordSeq = DOTween.Sequence();
-            shieldSeq = DOTween.Sequence();
             ChangeOpacity(1, 0, 0);
         }
         private void ChangeOpacity(float from, float to, float duration) => ChangeOpacity(from, to, duration, duration);
         private void ChangeOpacity(float from, float to, float swordDuration, float shieldDuration)
         {
-            if (swordSeq.IsPlaying()) swordSeq.Kill();
+            if(Sword != null) DOTween.Pause(Sword);
 
             float value1 = 0;
-            swordSeq.Append(
-                DOTween.To(() => value1, x => value1 = x, to, swordDuration).From(from).OnUpdate(() => {
-                upArrowMat.SetFloat("_Opacity", value1);
+            Sword = DOTween.To(() => value1, x => value1 = x, to, swordDuration).From(from).OnUpdate(() =>
+            {
+                if (_UpArrow != null) upArrowMat.SetFloat("_Opacity", value1);
                 whipMat.SetFloat("_Opacity", value1);
-                })
-            );
-            swordSeq.Play();
+            });
 
-            if (shieldSeq.IsPlaying()) swordSeq.Kill();
-
+            if (Shield != null) DOTween.Pause(Shield);
             float value2 = 0;
-            shieldSeq.Append(
-                DOTween.To(() => value2, x => value2 = x, to, shieldDuration).From(from).OnUpdate(() => {
-                    downArrowMat.SetFloat("_Opacity", value2);
-                    shieldMat.SetFloat("_Opacity", value2);
-                })
-            );
-            shieldSeq.Play();
+
+            Shield = DOTween.To(() => value2, x => value2 = x, to, shieldDuration).From(from).OnUpdate(() =>
+            {
+                if (_DownArrow != null) downArrowMat.SetFloat("_Opacity", value2);
+                shieldMat.SetFloat("_Opacity", value2);
+            });
+
+            if (_DuoArrow == null) return;
+
+            if(DuoArrow != null) DOTween.Pause(DuoArrow);
+            float value3 = 0;
+            float duration = (swordDuration > shieldDuration) ? shieldDuration : swordDuration;
+            DuoArrow = DOTween.To(() => value3, x => value3 = x, to, duration).From(from).OnUpdate(() =>
+            {
+                duoArrowMat.SetFloat("_Opacity", value3);
+            });
         }
         public void ShowReminder() {
             if (!gameObject.activeSelf) return;
             if (_ReminderState == State.ShowUI) return;
             _ReminderState = State.ShowUI;
             _ShowReminder.OnEnable();
-
+            print("Show Reminder");
             ChangeOpacity(0, 1, .1f);
         }
         public void HideReminder()
@@ -76,7 +88,7 @@ namespace HelloPico2.LevelTool
             if (_ReminderState == State.HideUI) return;
             _ReminderState = State.HideUI;
             _HideReminder.OnEnable();
-
+            print("Hide Reminder");
             ChangeOpacity(1, 0, .3f);
         }
         public void UseSword() {
@@ -84,8 +96,8 @@ namespace HelloPico2.LevelTool
             if (_ReminderState == State.HideUI) return;
             _ReminderState= State.HideUI;
             _UseSword.OnEnable();
-
-            ChangeOpacity(1, 0, .5f, .1f);
+            print("UseSword");
+            ChangeOpacity(1, 0, _IconFadeInDuration, _IconFadeOutDuration);
         }
         public void UseShield()
         {
@@ -93,8 +105,8 @@ namespace HelloPico2.LevelTool
             if (_ReminderState == State.HideUI) return;
             _ReminderState = State.HideUI;
             _UseShield.OnEnable();
-
-            ChangeOpacity(1, 0, .1f, .5f);
+            print("UseShield");
+            ChangeOpacity(1, 0, _IconFadeOutDuration, _IconFadeInDuration);
         }
     }
 }
