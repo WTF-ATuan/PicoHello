e00342ad103ddf206c20703f807c445ff6865ea1
Andy <andy@storm8.com>
2023-02-16 19:11:52 -0800

    Logo image sequence update

--- Assets/HelloPico2/LevelTool/MeshTextureSequence.cs
+++ Assets/HelloPico2/LevelTool/MeshTextureSequence.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using AV.Inspector.Runtime;
+using System.Collections;
 using System.Collections.Generic;
 using UltEvents;
 using UnityEngine;
+using UnityEngine.InputSystem.iOS;
 
 namespace HelloPico2.LevelTool
 {
@@ -19,9 +21,17 @@ namespace HelloPico2.LevelTool
         [SerializeField] private int _FPS;
         [SerializeField] private bool _Loop;
         [SerializeField] private bool _CanInterupt;
-        public UltEvent WhenFinished;
-        Coroutine process;
 
+        public UltEvent WhenFinished;
+
+        //[ReadOnly][SerializeField] private float _totalPlayTime;
+        [ReadOnly][SerializeField] private Texture _currentTexture;
+        Coroutine process;
+        public void OnValidate()
+        {
+            //var textures = _LogoImageSequence.Find(x => x.language == _UseLanguage).textureSequence;
+            //_totalPlayTime = (1f / (float)_FPS) * textures.Length;
+        }
         private void Start(){
             ChangeLanguage(Application.systemLanguage == SystemLanguage.ChineseSimplified ? Language.CN : Language.EN);
         }
@@ -39,17 +49,22 @@ namespace HelloPico2.LevelTool
         }
         private IEnumerator TextureSequencer() { 
             int index = 0;
-            Texture[] textures = _LogoImageSequence.Find(x => x.language == _UseLanguage).textureSequence;
-
-            while (index < textures.Length || _Loop) {            
+            //Texture[] textures = _LogoImageSequence.Find(x => x.language == _UseLanguage).textureSequence;
+            List<Texture> textures = new List<Texture>(_LogoImageSequence.Find(x => x.language == _UseLanguage).textureSequence);
+            var start = Time.time;
+            while (index < textures.Count || _Loop) {            
                 _MeshRenderer.material.mainTexture = textures[index];
+                _currentTexture = textures[index];
                 index++;
-                if(index >= textures.Length && _Loop) {                     
+                if(index >= textures.Count && _Loop) {                     
                     index = 0; 
                 }
                 yield return new WaitForSeconds(1f/(float)_FPS);
+                //yield return new WaitForEndOfFrame();
             }
+
             WhenFinished?.InvokeSafe();
+            //print("Stop Playing " + (Time.time - start));
         }
     }
 }
