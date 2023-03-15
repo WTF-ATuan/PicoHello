c8501396da1c75cca2bef064da5aac197122e2d3
Atuan <jain60501@gmail.com.tw>
2023-03-15 15:59:28 +0800

    reduce GC in armCon

--- Assets/HelloPico2/InteractableObjects/Scripts/HitTarget/FollowParticle.cs
+++ Assets/HelloPico2/InteractableObjects/Scripts/HitTarget/FollowParticle.cs
@@ -3,108 +3,99 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
-namespace HelloPico2.InteractableObjects
-{
-    public class FollowParticle : MonoBehaviour
-    {
-        public bool Activate;
-        public bool UseWorldPosition = false;
-        public bool SpawnFollowerWhenBirth = false;
-        [ShowIf("SpawnFollowerWhenBirth")] public int SpawnAmount = 10;
-        public bool DisableFollowerOnAwake = true;
-        public GameObject[] m_Follower;
-        public ParticleSystem m_FollowThis;
-        public bool m_DeactivateAfterParticleDie = true;
-        [ShowIf("m_DeactivateAfterParticleDie")] public float m_DelayDeactiveTime = .5f;
-        ParticleSystem.Particle[] m_Particles;
-        public System.Action<GameObject> WhenParticleDies;
+namespace HelloPico2.InteractableObjects{
+	public class FollowParticle : MonoBehaviour{
+		public bool Activate;
+		public bool UseWorldPosition = false;
+		public bool SpawnFollowerWhenBirth = false;
+		[ShowIf("SpawnFollowerWhenBirth")] public int SpawnAmount = 10;
+		public bool DisableFollowerOnAwake = true;
+		public GameObject[] m_Follower;
+		public ParticleSystem m_FollowThis;
+		public bool m_DeactivateAfterParticleDie = true;
 
-        public void ActivateFollowParticle() {
-            Activate = true;
-        }
-        private void OnValidate()
-        {
-            if (m_Follower.Length != 0)
-            {
-                for (int i = 0; i < m_Follower.Length; i++)
-                {
-                    if (m_Follower[i] == gameObject)
-                        throw new System.Exception("Can't assign self to m_Follower.");
-                }
-            }
-        }
-        private void Awake()
-        {
-            for (int i = 0; i < m_Follower.Length; i++)
-            {
-                m_Follower[i].SetActive(!DisableFollowerOnAwake);
-            }            
-        }
-        private void OnEnable()
-        {
-            if (SpawnFollowerWhenBirth)
-            {
-                List<GameObject> cloneList = new List<GameObject>();
+		[ShowIf("m_DeactivateAfterParticleDie")]
+		public float m_DelayDeactiveTime = .5f;
 
-                var obj = m_Follower[0];
-                cloneList.Add(obj);
+		ParticleSystem.Particle[] m_Particles;
+		public System.Action<GameObject> WhenParticleDies;
 
-                for (int i = 0; i < SpawnAmount; i++)
-                {
-                    var clone = Instantiate(obj);
-                    cloneList.Add(clone);
-                    clone.transform.SetParent(transform);
-                    clone.SetActive(!DisableFollowerOnAwake);
-                }
+		public void ActivateFollowParticle(){
+			Activate = true;
+		}
 
-                m_Follower = cloneList.ToArray();
-            }
-        }
-        public void Update()
-        {
-            if (!Activate) return;
-            
-            m_Particles = new ParticleSystem.Particle[m_FollowThis.main.maxParticles];
-            int particlesAliveAmount = m_FollowThis.GetParticles(m_Particles);
+		private void OnValidate(){
+			if(m_Follower.Length != 0){
+				for(int i = 0; i < m_Follower.Length; i++){
+					if(m_Follower[i] == gameObject)
+						throw new System.Exception("Can't assign self to m_Follower.");
+				}
+			}
+		}
 
-            for (int i = 0; i < particlesAliveAmount; i++)
-            {
-                if (i < m_Follower.Length)
-                {
-                    if (m_Follower[i] == null) continue;
+		private void Awake(){
+			for(int i = 0; i < m_Follower.Length; i++){
+				m_Follower[i].SetActive(!DisableFollowerOnAwake);
+			}
+		}
 
-                    if (UseWorldPosition)
-                        m_Follower[i].transform.position = m_Particles[i].position;
-                    else
-                        m_Follower[i].transform.position = m_Particles[i].position + m_FollowThis.transform.position;
+		private void OnEnable(){
+			if(SpawnFollowerWhenBirth){
+				List<GameObject> cloneList = new List<GameObject>();
 
-                    m_Follower[i].transform.forward = m_Particles[i].animatedVelocity.normalized;                    
+				var obj = m_Follower[0];
+				cloneList.Add(obj);
 
-                    if (m_Particles[i].remainingLifetime > 0.3f)
-                    {
-                        m_Follower[i].SetActive(true);
-                    }
-                    else
-                    {
-                        WhenParticleDies?.Invoke(m_Follower[i]);
+				for(int i = 0; i < SpawnAmount; i++){
+					var clone = Instantiate(obj);
+					cloneList.Add(clone);
+					clone.transform.SetParent(transform);
+					clone.SetActive(!DisableFollowerOnAwake);
+				}
 
-                        if (m_DeactivateAfterParticleDie)
-                            StartCoroutine(Delayer(m_Follower[i]));
+				m_Follower = cloneList.ToArray();
+			}
+		}
 
-                        m_Follower[i].SetActive(false);
-                        m_Follower[i].transform.position = m_FollowThis.transform.position;
-                    }
+		public void FixedUpdate(){
+			if(!Activate) return;
 
-                }
-            }
+			m_Particles = new ParticleSystem.Particle[m_FollowThis.main.maxParticles];
+			int particlesAliveAmount = m_FollowThis.GetParticles(m_Particles);
 
-            m_FollowThis.SetParticles(m_Particles, particlesAliveAmount);
-        }
-        private IEnumerator Delayer(GameObject follower)
-        {
-            yield return new WaitForSeconds(m_DelayDeactiveTime);
-            follower.SetActive(false);
-            follower.transform.position = m_FollowThis.transform.position;
-        }
-    }
+			for(int i = 0; i < particlesAliveAmount; i++){
+				if(i < m_Follower.Length){
+					if(m_Follower[i] == null) continue;
+
+					if(UseWorldPosition)
+						m_Follower[i].transform.position = m_Particles[i].position;
+					else
+						m_Follower[i].transform.position = m_Particles[i].position + m_FollowThis.transform.position;
+
+					m_Follower[i].transform.forward = m_Particles[i].animatedVelocity.normalized;
+
+					if(m_Particles[i].remainingLifetime > 0.3f){
+						m_Follower[i].SetActive(true);
+					}
+					else{
+						WhenParticleDies?.Invoke(m_Follower[i]);
+
+						if(m_DeactivateAfterParticleDie)
+							StartCoroutine(Delayer(m_Follower[i]));
+
+						m_Follower[i].SetActive(false);
+						m_Follower[i].transform.position = m_FollowThis.transform.position;
+					}
+				}
+			}
+
+			m_FollowThis.SetParticles(m_Particles, particlesAliveAmount);
+		}
+
+		private IEnumerator Delayer(GameObject follower){
+			yield return new WaitForSeconds(m_DelayDeactiveTime);
+			follower.SetActive(false);
+			follower.transform.position = m_FollowThis.transform.position;
+		}
+	}
 }
--- Assets/HelloPico2/PlayerController/Arm/Scripts/ArmConVisualEvents.cs
+++ Assets/HelloPico2/PlayerController/Arm/Scripts/ArmConVisualEvents.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,128 +8,123 @@ using HelloPico2.InteractableObjects;
 using HelloPico2.InputDevice.Scripts;
 using Unity.XR.PXR;
 
-namespace HelloPico2.PlayerController.Arm
-{
-    
-    public class ArmConVisualEvents : MonoBehaviour
-    {
-        public string _PrimaryAxisXName = "xAxis";
-        public string _PrimaryAxisYName = "yAxis";
-        public bool _InvertX = false;
-        public XRController _controller;
-        public UltEvents.UltEvent WhenTriggerTouch;
-        public UltEvents.UltEvent WhenTriggerNotTouch;
-        public UltEvents.UltEvent WhenGripTouch;
-        public UltEvents.UltEvent WhenGripNotTouch;
-        public UltEvents.UltEvent WhenJoyStickTouch;
-        public UltEvents.UltEvent WhenJoyStickNotTouch;
-        public UltEvents.UltEvent WhenJoyStickStay;
-        public UltEvents.UltEvent WhenJoyStickMove;
-        public UltEvents.UltEvent WhenJoyStickForward;
-        public UltEvents.UltEvent WhenJoyStickBackward;
-        public UltEvents.UltEvent WhenJoyStickIdle;
-        public UltEvents.UltEvent WhenPrimaryButtonTouch;
-        public UltEvents.UltEvent WhenPrimaryButtonNotTouch;
-        public UltEvents.UltEvent WhenSecondaryButtonTouch;
-        public UltEvents.UltEvent WhenSecondaryButtonNotTouch;
-        public UltEvents.UltEvent WhenPrimaryButtonDown;
-        public UltEvents.UltEvent WhenPrimaryButtonUp;
-        public UltEvents.UltEvent WhenSecondaryButtonDown;
-        public UltEvents.UltEvent WhenSecondaryButtonUp;
+namespace HelloPico2.PlayerController.Arm{
+	public class ArmConVisualEvents : MonoBehaviour{
+		public string _PrimaryAxisXName = "xAxis";
+		public string _PrimaryAxisYName = "yAxis";
+		public bool _InvertX = false;
+		public XRController _controller;
+		public UltEvents.UltEvent WhenTriggerTouch;
+		public UltEvents.UltEvent WhenTriggerNotTouch;
+		public UltEvents.UltEvent WhenGripTouch;
+		public UltEvents.UltEvent WhenGripNotTouch;
+		public UltEvents.UltEvent WhenJoyStickTouch;
+		public UltEvents.UltEvent WhenJoyStickNotTouch;
+		public UltEvents.UltEvent WhenJoyStickStay;
+		public UltEvents.UltEvent WhenJoyStickMove;
+		public UltEvents.UltEvent WhenJoyStickForward;
+		public UltEvents.UltEvent WhenJoyStickBackward;
+		public UltEvents.UltEvent WhenJoyStickIdle;
+		public UltEvents.UltEvent WhenPrimaryButtonTouch;
+		public UltEvents.UltEvent WhenPrimaryButtonNotTouch;
+		public UltEvents.UltEvent WhenSecondaryButtonTouch;
+		public UltEvents.UltEvent WhenSecondaryButtonNotTouch;
+		public UltEvents.UltEvent WhenPrimaryButtonDown;
+		public UltEvents.UltEvent WhenPrimaryButtonUp;
+		public UltEvents.UltEvent WhenSecondaryButtonDown;
+		public UltEvents.UltEvent WhenSecondaryButtonUp;
 
-        private Vector2 primary2DAxisValueCurrent;
+		private Vector2 primary2DAxisValueCurrent;
 
-        public float DeadValue = 0.3f;
-        private int invert;
+		public float DeadValue = 0.3f;
+		private int invert;
+		private bool _isTrigger;
+		private bool _isGrip;
+		private bool _padAxisTouch;
+		private bool _primaryButtonValue;
+		private bool _secondaryButtonValue;
+		private bool _primaryTouchValue;
+		private bool _secondaryTouchValue;
+		private Vector2 _primary2DAxisValue;
+		
+		private AnimatorValueChanger _animatorValueChanger;
 
-        private void Update()
-        {
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out var isTrigger);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.gripButton, out var isGrip);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggetValue);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxisTouch, out var padAxisTouch);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out var padAxisClick);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondaryButtonValue);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.secondaryTouch, out bool secondaryTouchValue);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
-            _controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryTouch, out bool primaryTouchValue);
-            _controller.inputDevice.TryGetFeatureValue(PXR_Usages.grip1DAxis, out var grip1DAxis);
+		private void Start(){
+			_animatorValueChanger = GetComponent<AnimatorValueChanger>();
+		}
 
-            if (TryGetComponent<AnimatorValueChanger>(out var valueChanger))
-            {
-                invert = (_InvertX) ? -1 : 1;
-                valueChanger.animator.SetFloat(_PrimaryAxisXName, primary2DAxisValue.x * invert);
-                valueChanger.animator.SetFloat(_PrimaryAxisYName, primary2DAxisValue.y);
-            }
+		private void Update(){
+			_controller.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out _isTrigger);
+			_controller.inputDevice.TryGetFeatureValue(CommonUsages.gripButton, out _isGrip);
+			_controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out _primary2DAxisValue);
+			_controller.inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out _secondaryButtonValue);
+			_controller.inputDevice.TryGetFeatureValue(CommonUsages.secondaryTouch, out _secondaryTouchValue);
+			_controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out _primaryButtonValue);
+			_controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryTouch, out _primaryTouchValue);
 
-            if (isTrigger)
-                WhenTriggerTouch?.Invoke();
-            else
-                WhenTriggerNotTouch?.Invoke();
+			if(_animatorValueChanger){
+				invert = (_InvertX) ? -1 : 1;
+				_animatorValueChanger.animator.SetFloat(_PrimaryAxisXName, _primary2DAxisValue.x * invert);
+				_animatorValueChanger.animator.SetFloat(_PrimaryAxisYName, _primary2DAxisValue.y);
+			}
 
-            if(isGrip)
-                WhenGripTouch?.Invoke();
-            else
-                WhenGripNotTouch?.Invoke();
+			if(_isTrigger)
+				WhenTriggerTouch?.Invoke();
+			else
+				WhenTriggerNotTouch?.Invoke();
 
-            if (padAxisTouch)
-                WhenJoyStickTouch?.Invoke();
-            else
-                WhenJoyStickNotTouch?.Invoke();
+			if(_isGrip)
+				WhenGripTouch?.Invoke();
+			else
+				WhenGripNotTouch?.Invoke();
 
-            //if(primaryTouchValue)
-            //    WhenPrimaryButtonTouch?.Invoke();
-            //else
-            //    WhenPrimaryButtonNotTouch?.Invoke();    
+			if(_padAxisTouch)
+				WhenJoyStickTouch?.Invoke();
+			else
+				WhenJoyStickNotTouch?.Invoke();
 
-            //if(secondaryTouchValue)
-            //    WhenSecondaryButtonTouch?.Invoke(); 
-            //else
-            //    WhenSecondaryButtonNotTouch?.Invoke();
+			if(_primaryButtonValue)
+				WhenPrimaryButtonDown?.Invoke();
+			else
+				WhenPrimaryButtonUp?.Invoke();
 
-            if (primaryButtonValue)
-                WhenPrimaryButtonDown?.Invoke();
-            else
-                WhenPrimaryButtonUp?.Invoke();
+			if(_secondaryButtonValue)
+				WhenSecondaryButtonDown?.Invoke();
+			else
+				WhenSecondaryButtonUp?.Invoke();
 
-            if (secondaryButtonValue)
-                WhenSecondaryButtonDown?.Invoke();
-            else
-                WhenSecondaryButtonUp?.Invoke();
+			if(_primaryTouchValue && !_secondaryTouchValue)
+				WhenPrimaryButtonTouch?.Invoke();
+			else if(!_primaryTouchValue)
+				WhenPrimaryButtonNotTouch?.Invoke();
 
-            if (primaryTouchValue && !secondaryTouchValue)
-                WhenPrimaryButtonTouch?.Invoke();
-            else if (!primaryTouchValue)
-                WhenPrimaryButtonNotTouch?.Invoke();
+			if(_secondaryTouchValue){
+				WhenSecondaryButtonTouch?.Invoke();
+				WhenPrimaryButtonNotTouch?.Invoke();
+			}
 
-            if (secondaryTouchValue)
-                { WhenSecondaryButtonTouch?.Invoke(); WhenPrimaryButtonNotTouch?.Invoke(); }
-            if (!secondaryTouchValue)
-                WhenSecondaryButtonNotTouch?.Invoke();
+			if(!_secondaryTouchValue)
+				WhenSecondaryButtonNotTouch?.Invoke();
 
-            if (Mathf.Abs(primary2DAxisValue.x) >= 0.1f || Mathf.Abs(primary2DAxisValue.y) >= 0.1f)
-            {
-                WhenJoyStickMove?.Invoke();
-            }
-            if (Mathf.Abs(primary2DAxisValue.x) <= 0.1f && Mathf.Abs(primary2DAxisValue.y) <= 0.1f)
-            {
-                WhenJoyStickStay?.Invoke();
-            }
-            
-            if (primary2DAxisValue.y > DeadValue)
-            {
-                WhenJoyStickForward?.Invoke();
-            }
-            if (Mathf.Abs(primary2DAxisValue.y) <= DeadValue && padAxisTouch)
-            {
-                WhenJoyStickIdle?.Invoke();
-            }
-            if (primary2DAxisValue.y < -DeadValue)
-            {
-                WhenJoyStickBackward?.Invoke();
-            }
-        }
-    }
+			if(Mathf.Abs(_primary2DAxisValue.x) >= 0.1f || Mathf.Abs(_primary2DAxisValue.y) >= 0.1f){
+				WhenJoyStickMove?.Invoke();
+			}
+
+			if(Mathf.Abs(_primary2DAxisValue.x) <= 0.1f && Mathf.Abs(_primary2DAxisValue.y) <= 0.1f){
+				WhenJoyStickStay?.Invoke();
+			}
+
+			if(_primary2DAxisValue.y > DeadValue){
+				WhenJoyStickForward?.Invoke();
+			}
+
+			if(Mathf.Abs(_primary2DAxisValue.y) <= DeadValue && _padAxisTouch){
+				WhenJoyStickIdle?.Invoke();
+			}
+
+			if(_primary2DAxisValue.y < -DeadValue){
+				WhenJoyStickBackward?.Invoke();
+			}
+		}
+	}
 }
